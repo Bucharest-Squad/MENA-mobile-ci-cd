cd82ff27c909988f206c83ae2b73167478bfab3b
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-09-10 18:35:51 +0100

    refactor: remove AppLogger and its implementations

--- composeApp/src/commonMain/kotlin/net/thechance/mena/AppLogger.kt
+++
@@ -1,7 +0,0 @@
-package net.thechance.mena
-
-expect object AppLogger {
-    fun d(message: String, tag: String?)
-    fun i(message: String, tag: String?)
-    fun e(message: String, throwable: Throwable?, tag: String?)
-}
