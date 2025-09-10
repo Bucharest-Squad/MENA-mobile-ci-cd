33624bfad6d78192bd9eaa3ff4eea52c93676de3
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-10 12:00:51 +0300

    Refactor: Improve error message in BaseRepository
    Updated the error message in `BaseRepository.kt` to "This line should never be reached" for better clarity when an unknown error occurs.

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/shared/BaseRepository.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/shared/BaseRepository.kt
@@ -24,7 +24,7 @@ interface BaseRepository {
                 }
             }
         }
-        throw ChatException("Unknown error") // This line should never be reached
+        throw ChatException("This line should never be reached")
     }
 
     suspend fun <T> runCatchingWithException(
