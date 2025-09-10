b6124de6b4cc001bff22b147db0629cf32c48d60
haider <haideraljawasim@gmail.com>
2025-09-10 20:47:54 +0300

    add no Internet exception

--- dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/exceptions/Exceptions.kt
+++ dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/exceptions/Exceptions.kt
@@ -4,3 +4,4 @@ open class DukanException(message: String = "Dukan Exception") : Exception(messa
 
 class DukanNotFoundException(message: String = "Dukan Not Found") : DukanException(message)
 class DukanNameTakenException(message: String = "Dukan Name Taken") : DukanException(message)
+class NoInternetException(message: String = "No Internet") : DukanException(message)
