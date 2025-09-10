d24b6718c424be2add4a75d83eca5430ffc6993d
Aboud <gh.abd.com.207@gmail.com>
2025-09-10 08:53:34 -0700

    remove jvm from domains

--- wallet_domain/build.gradle.kts
+++ wallet_domain/build.gradle.kts
@@ -1,16 +1,8 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
     iosArm64()
     iosSimulatorArm64()
 
@@ -18,13 +10,5 @@ kotlin {
         commonMain.dependencies {
 
         }
-    }
-}
-android {
-    namespace = "net.thechance.mena.wallet.domain"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
