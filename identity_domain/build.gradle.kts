32df647c228bce144cfaeb24da878b4588d554eb
Aboud <gh.abd.com.207@gmail.com>
2025-09-10 03:55:55 -0700

    Add android target to domain modules

--- identity_domain/build.gradle.kts
+++ identity_domain/build.gradle.kts
@@ -1,8 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
     iosArm64()
     iosSimulatorArm64()
 
@@ -10,5 +18,13 @@ kotlin {
         commonMain.dependencies {
 
         }
+    }
+}
+android {
+    namespace = "net.thechance.mena.identity.domain"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
