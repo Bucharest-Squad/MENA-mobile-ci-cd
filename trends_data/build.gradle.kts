888459d0a4e4c4734fee3d7dd411685c92b83e3d
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-09-11 19:18:19 +0300

    feature: change gradle for data to test ci cd test coverage

--- trends_data/build.gradle.kts
+++ trends_data/build.gradle.kts
@@ -1,12 +1,21 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kover)
+    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
     jvm()
     iosArm64()
     iosSimulatorArm64()
+
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
 
     sourceSets {
         androidMain.dependencies {
