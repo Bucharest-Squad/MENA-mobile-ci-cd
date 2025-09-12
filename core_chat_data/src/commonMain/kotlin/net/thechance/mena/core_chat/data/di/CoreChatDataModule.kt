6c64b01eefdfd0b4c618acb0c920bf7d6a8884cb
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-09-12 11:23:09 +0300

    Update Koin factories to singletons
    Changed `factoryOf` to `singleOf` in `CoreChatPresentationModule.kt` and `CoreChatDataModule.kt` for `CoreChatApiImp`, `DummyContactsDataSource`, and `ContactsRepositoryImpl`. This ensures that these dependencies are created as singletons, providing a single instance throughout the application lifecycle.

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/CoreChatDataModule.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/CoreChatDataModule.kt
@@ -4,10 +4,10 @@ import org.koin.dsl.module
 import net.thechance.mena.core_chat.data.contacts.ContactsRepositoryImpl
 import net.thechance.mena.core_chat.data.contacts.source.remote.DummyContactsDataSource
 import net.thechance.mena.core_chat.domain.repository.ContactsRepository
-import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 
 val coreChatDataModule = module {
-    factoryOf(::DummyContactsDataSource)
-    factoryOf(::ContactsRepositoryImpl) bind ContactsRepository::class
+    singleOf(::DummyContactsDataSource)
+    singleOf(::ContactsRepositoryImpl) bind ContactsRepository::class
 }
--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/di/CoreChatPresentationModule.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/di/CoreChatPresentationModule.kt
@@ -5,12 +5,12 @@ import net.thechance.mena.core_chat.api.CoreChatApi
 import net.thechance.mena.core_chat.presentation.api.CoreChatApiImp
 import net.thechance.mena.core_chat.presentation.screen.contacts.ContactsViewModel
 import net.thechance.mena.core_chat.presentation.screen.syncContacts.SyncContactsViewModel
-import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 
 val coreChatPresentationModule = module {
-    factoryOf(::CoreChatApiImp) bind CoreChatApi::class
+    singleOf(::CoreChatApiImp) bind CoreChatApi::class
     viewModelOf(::ContactsViewModel)
     viewModelOf(::SyncContactsViewModel)
 }
