50d865e8aa9bdd431eee09dae4b810f4ea1703b1
bilalazzam <blalazam065@gmail.com>
2025-09-12 04:09:07 +0300

    add device contacts datasource dependency

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/contacts/ContactMapper.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/contacts/ContactMapper.kt
@@ -6,6 +6,7 @@ import net.thechance.mena.core_chat.data.shared.dto.PagedDataDto
 import net.thechance.mena.core_chat.domain.entity.Contact
 import net.thechance.mena.core_chat.domain.exception.ContactsFetchFailedException
 import net.thechance.mena.core_chat.domain.model.PagedData
+import com.bilalazzam.contacts_provider.Contact as DeviceContact
 
 fun PagedDataDto<ContactDto>?.toPagedListOfContacts(): PagedData<Contact> {
     val pagedData = this ?: throw ContactsFetchFailedException("Response body is null")
@@ -37,5 +38,28 @@ private fun Contact.toContactCreationRequestDto(): ContactCreationRequestDto {
     return ContactCreationRequestDto(
         name = name,
         phone = phone
+    )
+}
+
+fun List<DeviceContact>.toListOfContacts(): List<Contact> {
+    return flatMap { deviceContact ->
+        deviceContact.phoneNumbers.map { phone ->
+            Contact(
+                name = listOfNotNull(deviceContact.firstName, deviceContact.lastName)
+                    .joinToString(" "),
+                phone = phone,
+                isMenaUser = false,
+                imageUrl = null
+            )
+        }
+    }
+}
+
+fun List<DeviceContact>.toPagedListOfContacts(): PagedData<Contact> {
+    val contacts = this.toListOfContacts()
+    return PagedData(
+        data = contacts,
+        totalItems = contacts.size,
+        isLastPage = true
     )
 }
--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/contacts/ContactsRepositoryImpl.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/contacts/ContactsRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package net.thechance.mena.core_chat.data.contacts
 
+import net.thechance.mena.core_chat.data.contacts.source.device.DeviceContactsDataSource
 import net.thechance.mena.core_chat.data.contacts.source.remote.DummyContactsDataSource
 import net.thechance.mena.core_chat.data.shared.BaseRepository
 import net.thechance.mena.core_chat.domain.entity.Contact
@@ -9,17 +10,26 @@ import net.thechance.mena.core_chat.domain.model.PagedData
 import net.thechance.mena.core_chat.domain.repository.ContactsRepository
 
 class ContactsRepositoryImpl(
-    private val contactsDataSource: DummyContactsDataSource
+    private val contactsDataSource: DummyContactsDataSource,
+    private val deviceContactsDatasource: DeviceContactsDataSource
 ) : ContactsRepository, BaseRepository {
 
     override suspend fun getUserContacts(pageNumber: Int, pageSize: Int): PagedData<Contact> {
-        return tryNetworkCall(
+        val remoteContacts = tryNetworkCall(
             defaultException = { ContactsFetchFailedException("Couldn't get user contacts", it) }) {
             contactsDataSource.getUserContacts(
                 pageNumber = pageNumber,
                 pageSize = pageSize
             )
         }.toPagedListOfContacts()
+
+        if (remoteContacts.data.isEmpty()) {
+            return deviceContactsDatasource.getDeviceContacts().also {
+                syncContacts(it.toListOfContacts())
+            }.toPagedListOfContacts()
+        }
+
+        return remoteContacts
     }
 
     override suspend fun syncContacts(contacts: List<Contact>) {
