79d6fae9aa254c5ff8e392d65bb3f33f0a2da5a6
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-09-13 00:36:56 +0300

    feature: add test for CoreTestHandler

---
+++ core_chat_data/src/commonTest/kotlin/net/thechance/mena/core_chat/data/CoreTestHandlerTest.kt
@@ -0,0 +1,11 @@
+package net.thechance.mena.core_chat.data
+
+import kotlin.test.Test
+
+class CoreTestHandlerTest {
+
+    @Test
+    fun testCore() {
+        CoreTestHandler().testCore()
+    }
+}
