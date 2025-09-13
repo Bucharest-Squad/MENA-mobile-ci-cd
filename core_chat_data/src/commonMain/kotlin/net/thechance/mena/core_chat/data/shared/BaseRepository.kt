edfb50a92470fcabd9e6287dfbbee4335e414e9d
bilalazzam <blalazam065@gmail.com>
2025-09-13 14:50:24 +0300

    handle contacts provider exception

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/shared/BaseRepository.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/shared/BaseRepository.kt
@@ -1,7 +1,11 @@
 package net.thechance.mena.core_chat.data.shared
 
+import com.bilalazzam.contacts_provider.ContactsPermissionDeniedException as ContactsProviderPermissionDeniedException
+import com.bilalazzam.contacts_provider.FetchContactsFailedException
 import net.thechance.mena.core_chat.data.shared.dto.BaseResponseDto
 import net.thechance.mena.core_chat.domain.exception.ChatException
+import net.thechance.mena.core_chat.domain.exception.ContactsFetchFailedException
+import net.thechance.mena.core_chat.domain.exception.ContactsPermissionDeniedException
 import net.thechance.mena.core_chat.domain.exception.UnAuthorizedException
 import net.thechance.mena.core_chat.domain.exception.UnknownException
 
@@ -40,6 +44,10 @@ interface BaseRepository {
     ): T {
         try {
             return block()
+        } catch (e: ContactsProviderPermissionDeniedException) {
+            throw ContactsPermissionDeniedException("Contacts Permission Denied!", e)
+        } catch (e: FetchContactsFailedException) {
+            throw ContactsFetchFailedException("Failed to fetch Contacts!", e)
         } catch (e: ChatException) {
             throw e
         } catch (e: Exception) {
