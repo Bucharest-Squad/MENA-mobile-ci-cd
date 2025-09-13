b6e2ad476600b5bdcaaf6fe942300d97c08364f5
AhmadKharfan <plamoplamo12@gmail.com>
2025-09-13 15:14:57 +0000

    test code cov

---
+++ core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/ClassA.kt
@@ -0,0 +1,11 @@
+package net.thechance.mena.core_chat.domain
+
+class Dummy {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun isEven(number: Int): Boolean {
+        return number % 2 == 0
+    }
+}
---
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/Dummy.kt
@@ -0,0 +1,11 @@
+package net.thechance.mena.wallet.data
+
+class Dummy {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun isEven(number: Int): Boolean {
+        return number % 2 == 0
+    }
+}
