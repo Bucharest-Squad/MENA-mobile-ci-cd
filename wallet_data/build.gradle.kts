2814ea352647b61fdeaa0959bf04bbf7360fe787
Hager Abobakr <hagarabobakr1@gmail.com>
2025-09-13 21:49:20 +0300

    add Ktor dependencies

--- wallet_data/build.gradle.kts
+++ wallet_data/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
@@ -8,13 +12,21 @@ kotlin {
 
     sourceSets {
         androidMain.dependencies {
+            //ktor
+            implementation(libs.ktor.client.android)
+            implementation(libs.ktor.client.okhttp)
 
         }
         commonMain.dependencies {
             implementation(projects.walletDomain)
+
+            //ktor
+            implementation(libs.bundles.ktor)
         }
         iosMain.dependencies {
 
+            //ktor
+            implementation(libs.ktor.client.darwin)
         }
     }
 }
