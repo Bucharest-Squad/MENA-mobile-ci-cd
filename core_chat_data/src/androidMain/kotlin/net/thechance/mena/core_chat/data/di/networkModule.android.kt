0f9d9410cfbc78bd2dfc2c9a7f488c7c93ffa690
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-09-13 23:34:11 +0300

    rename platform specific files to match the new structure

---
+++ core_chat_data/src/androidMain/kotlin/net/thechance/mena/core_chat/data/di/networkModule.android.kt
@@ -0,0 +1,10 @@
+package net.thechance.mena.core_chat.data.di
+
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.okhttp.OkHttp
+import org.koin.core.scope.Scope
+
+actual fun Scope.createHttpClientEngine(): HttpClientEngineFactory<HttpClientEngineConfig> {
+    return OkHttp
+}
