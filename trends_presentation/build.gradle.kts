0145d6ac6b3b086dee3d50c8abfabc5605b58d34
ahmbd <ahmbdelkhalek@gmail.com>
2025-09-13 11:35:50 +0300

    refactor: Update kotlinx-serialization-json to version catalog

--- trends_presentation/build.gradle.kts
+++ trends_presentation/build.gradle.kts
@@ -47,7 +47,7 @@ kotlin {
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
             api(libs.koin.annotations)
-            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+            implementation(libs.kotlinx.serialization.json)
         }
         iosMain.dependencies {
 
