1fe171ca53841b36a9bb581acfbb534928961ec1
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-09-13 17:46:33 +0300

    TRN refactor: update Tag component to use MenaText
    This commit refactors the `Tag` composable function.
    It now utilizes the `MenaText` component for displaying text and emoji.
    The `selected` parameter has been renamed to `isSelected` for clarity.
    The `onClick` lambda now takes no arguments, and its invocation has been updated accordingly.
    The preview function has also been updated to reflect these changes.

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/Tag.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/Tag.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +15,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import net.thechance.mena.designsystem.presentation.component.button.radioButton.RadioButton
+import net.thechance.mena.designsystem.presentation.component.text.MenaText
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -23,53 +24,51 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun Tag(
     text: String,
     emoji: String,
-    selected: Boolean,
+    isSelected: Boolean,
     modifier: Modifier = Modifier,
-    onClick: (Boolean) -> Unit = {}
+    onClick: () -> Unit
 ) {
     val textColor by animateColorAsState(
-        targetValue = if (selected) Theme.colorScheme.shadePrimary else Theme.colorScheme.shadeSecondary
+        targetValue = if (isSelected) Theme.colorScheme.shadePrimary else Theme.colorScheme.shadeSecondary
     )
     Row(
         horizontalArrangement = Arrangement.spacedBy(Theme.spacing._8),
         modifier = modifier
             .clip(RoundedCornerShape(Theme.radius.full))
             .background(color = Theme.colorScheme.primary.onPrimary)
-            .clickable {
-                onClick(!selected)
-            }
+            .clickable { onClick() }
             .padding(10.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(Theme.spacing._4)
         ) {
-            Text(
-                text = emoji
+            MenaText(
+                text = emoji,
+                style = Theme.typography.label.extraSmall
             )
-            Text(
+            MenaText(
                 text = text,
-                color = textColor
+                color = textColor,
+                style = Theme.typography.label.extraSmall // TODO: Should be Label/XSmall
             )
         }
-        net.thechance.mena.designsystem.presentation.component.button.radioButton.RadioButton(
-            isSelected = selected,
-            onClick = {
-                onClick(!selected)
-            },
+        RadioButton(
+            isSelected = isSelected,
+            onClick = onClick,
         )
     }
 }
 
 @Preview
 @Composable
-fun TagPreview() {
-    var selected by remember { mutableStateOf(false) }
+private fun TagPreview() {
+    var isSelected by remember { mutableStateOf(false) }
     Tag(
         text = "Hello",
         emoji = "\uD83D\uDE00",
-        selected = selected,
+        isSelected = isSelected,
         onClick = {
-            selected = it
+            isSelected = isSelected.not()
         }
     )
 }
