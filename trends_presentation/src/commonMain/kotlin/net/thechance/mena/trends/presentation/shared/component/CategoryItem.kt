35a7d2739d907f02ecadfc8e31311dbb3b6ea0f3
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-09-13 21:31:39 +0300

    TRN refactor: rename Tag component to CategoryItem and add noRippleClickable modifier
    The `Tag` composable function has been renamed to `CategoryItem`.
    
    A new modifier extension `noRippleClickable` has been added. This modifier is used in `CategoryItem` to disable the ripple effect on click.
    
    The internal structure of `CategoryItem` has been slightly refactored by extracting the category name and emoji display into a private `CategoryNameAndEmoji` composable function for better readability.

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/CategoryItem.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/CategoryItem.kt
@@ -2,7 +2,6 @@ package net.thechance.mena.trends.presentation.shared.component
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -12,16 +11,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import net.thechance.mena.designsystem.presentation.component.button.radioButton.RadioButton
 import net.thechance.mena.designsystem.presentation.component.text.MenaText
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import net.thechance.mena.trends.presentation.shared.component.modifier.noRippleClickable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-fun Tag(
+fun CategoryItem(
     text: String,
     emoji: String,
     isSelected: Boolean,
@@ -33,25 +35,18 @@ fun Tag(
     )
     Row(
         horizontalArrangement = Arrangement.spacedBy(Theme.spacing._8),
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .clip(RoundedCornerShape(Theme.radius.full))
             .background(color = Theme.colorScheme.primary.onPrimary)
-            .clickable { onClick() }
+            .noRippleClickable { onClick() }
             .padding(10.dp)
     ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(Theme.spacing._4)
-        ) {
-            MenaText(
-                text = emoji,
-                style = Theme.typography.label.extraSmall
-            )
-            MenaText(
-                text = text,
-                color = textColor,
-                style = Theme.typography.label.extraSmall // TODO: Should be Label/XSmall
-            )
-        }
+        CategoryNameAndEmoji(
+            emoji = emoji,
+            text = text,
+            textColor = textColor
+        )
         RadioButton(
             isSelected = isSelected,
             onClick = onClick,
@@ -59,11 +54,33 @@ fun Tag(
     }
 }
 
+@Composable
+private fun CategoryNameAndEmoji(
+    emoji: String,
+    text: String,
+    textColor: Color
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(Theme.spacing._4),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        MenaText(
+            text = emoji,
+            style = Theme.typography.label.extraSmall
+        )
+        MenaText(
+            text = text,
+            color = textColor,
+            style = Theme.typography.label.extraSmall // TODO: Should be Label/XSmall
+        )
+    }
+}
+
 @Preview
 @Composable
-private fun TagPreview() {
+private fun CategoryItemPreview() {
     var isSelected by remember { mutableStateOf(false) }
-    Tag(
+    CategoryItem(
         text = "Hello",
         emoji = "\uD83D\uDE00",
         isSelected = isSelected,
