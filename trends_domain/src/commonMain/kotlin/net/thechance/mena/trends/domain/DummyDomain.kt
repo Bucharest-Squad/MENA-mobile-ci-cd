78b89872cda1c0cc47f58dd4898b85b4a2b1d847
AhmadKharfan <plamoplamo12@gmail.com>
2025-09-13 16:12:59 +0000

    test code cov

---
+++ trends_domain/src/commonMain/kotlin/net/thechance/mena/trends/domain/DummyDomain.kt
@@ -0,0 +1,13 @@
+package net.thechance.mena.trends.domain
+
+class DummyDomain(
+    val int: Int,
+    val string: String,
+    val boolean: Boolean,
+    val double: Double,
+    val float: Float,
+    val long: Long,
+    val short: Short,
+    val byte: Byte,
+    val char: Char
+)
