e66d3f0bbbe218902a19c2ba1ba35ce7ae7130f1
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-09-13 11:56:51 +0300

    change CoreChatApiImp binding from single to factory

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/di/ApiModule.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/di/ApiModule.kt
@@ -2,10 +2,10 @@ package net.thechance.mena.core_chat.presentation.di
 
 import net.thechance.mena.core_chat.api.CoreChatApi
 import net.thechance.mena.core_chat.presentation.api.CoreChatApiImp
-import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal val apiModule = module {
-    singleOf(::CoreChatApiImp) bind CoreChatApi::class
+    factoryOf(::CoreChatApiImp) bind CoreChatApi::class
 }
