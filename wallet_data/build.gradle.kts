a6ee22ec48bc9bb378c77c7014d648cb413bfa12
Hager Abobakr <hagarabobakr1@gmail.com>
2025-09-13 22:40:48 +0300

    make wallet_data a Kotlin Multiplatform library module

--- wallet_data/build.gradle.kts
+++ wallet_data/build.gradle.kts
@@ -1,12 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
     iosArm64()
     iosSimulatorArm64()
 
@@ -28,5 +33,14 @@ kotlin {
             //ktor
             implementation(libs.ktor.client.darwin)
         }
+    }
+}
+
+android {
+    namespace = "net.thechance.mena.wallet.data"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
