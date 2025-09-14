e14a61734d7036160b525b79b71e3c70ac37377b
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-09-14 18:25:28 +0300

    Refactor:Faith:17 Use constants for time units in `getTimeAgo`

--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/extensions/getTimeAgo.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/extensions/getTimeAgo.kt
@@ -22,39 +22,49 @@ fun Instant.getTimeAgo(): String {
     val duration: Duration = now - this
 
     return when {
-        duration.inWholeSeconds < 60 -> {
+        duration.inWholeSeconds < TimeConstants.ONE_MINUTE_IN_SECONDS -> {
             val seconds = duration.inWholeSeconds.toInt()
             pluralStringResource(Res.plurals.time_seconds_ago, seconds, seconds)
         }
 
-        duration.inWholeMinutes < 60 -> {
+        duration.inWholeMinutes < TimeConstants.ONE_HOUR_IN_MINUTES -> {
             val minutes = duration.inWholeMinutes.toInt()
             pluralStringResource(Res.plurals.time_minutes_ago, minutes, minutes)
         }
 
-        duration.inWholeHours < 24 -> {
+        duration.inWholeHours < TimeConstants.ONE_DAY_IN_HOURS -> {
             val hours = duration.inWholeHours.toInt()
             pluralStringResource(Res.plurals.time_hours_ago, hours, hours)
         }
 
-        duration.inWholeDays < 7 -> {
+        duration.inWholeDays < TimeConstants.ONE_WEEK_IN_DAYS -> {
             val days = duration.inWholeDays.toInt()
             pluralStringResource(Res.plurals.time_days_ago, days, days)
         }
 
-        duration.inWholeDays < 30 -> {
-            val weeks = (duration.inWholeDays / 7).toInt()
+        duration.inWholeDays < TimeConstants.ONE_MONTH_IN_DAYS -> {
+            val weeks = (duration.inWholeDays / TimeConstants.ONE_WEEK_IN_DAYS).toInt()
             pluralStringResource(Res.plurals.time_weeks_ago, weeks, weeks)
         }
 
-        duration.inWholeDays < 365 -> {
-            val months = (duration.inWholeDays / 30).toInt()
+        duration.inWholeDays < TimeConstants.ONE_YEAR_IN_DAYS -> {
+            val months = (duration.inWholeDays / TimeConstants.ONE_MONTH_IN_DAYS).toInt()
             pluralStringResource(Res.plurals.time_months_ago, months, months)
         }
 
         else -> {
-            val years = (duration.inWholeDays / 365).toInt()
+            val years = (duration.inWholeDays / TimeConstants.ONE_YEAR_IN_DAYS).toInt()
             pluralStringResource(Res.plurals.time_years_ago, years, years)
         }
     }
+}
+
+
+object TimeConstants {
+    const val ONE_MINUTE_IN_SECONDS = 60
+    const val ONE_HOUR_IN_MINUTES = 60
+    const val ONE_DAY_IN_HOURS = 24
+    const val ONE_WEEK_IN_DAYS = 7
+    const val ONE_MONTH_IN_DAYS = 30
+    const val ONE_YEAR_IN_DAYS = 365
 }
