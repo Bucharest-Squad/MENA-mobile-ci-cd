bf9a776a63a934df3b6f79c525ffaeb225fd3de2
Mona <monaayman584@gmail.com>
2025-09-14 13:45:29 +0300

    refactor: rename Avatar.kt to be CircularAvatar.kt and remove unneeded modifier

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screens/contacts/CircularAvatar.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screens/contacts/CircularAvatar.kt
@@ -4,39 +4,38 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Dp
 import coil3.compose.AsyncImage
+import net.thechance.mena.designsystem.presentation.component.text.MenaText
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 
 @Composable
-fun Avatar(
+fun CircularAvatar(
     contactImageUri: String?,
     contactInitials: String,
-    size: Int,
+    size: Dp,
     modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
-            .size(size.dp)
+            .size(size)
             .clip(CircleShape)
             .background(
                 color = Theme.colorScheme.background.surfaceLow
-            ) ,
+            ),
         contentAlignment = Alignment.Center
     ) {
         if (contactImageUri != null) {
             AsyncImage(
                 model = contactImageUri,
                 contentDescription = "Contact photo",
-                modifier = modifier
             )
         } else {
-            Text(
+            MenaText(
                 text = contactInitials,
                 color = Theme.colorScheme.shadePrimary,
                 style = Theme.typography.label.large,
