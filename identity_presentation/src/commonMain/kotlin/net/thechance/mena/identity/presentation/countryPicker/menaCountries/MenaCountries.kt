1656f5db319fefea8f4f15009de2a23501694a25
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-09-14 16:34:05 +0100

    refactor: use string resources in `MenaCountries`

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/countryPicker/menaCountries/MenaCountries.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/countryPicker/menaCountries/MenaCountries.kt
@@ -1,47 +1,73 @@
-package net.thechance.mena.identity.presentation.countryPicker.menaCountries;
+package net.thechance.mena.identity.presentation.countryPicker.menaCountries
 
 import mena.identity_presentation.generated.resources.Res
 import mena.identity_presentation.generated.resources.ae_flag
+import mena.identity_presentation.generated.resources.algeria
+import mena.identity_presentation.generated.resources.bahrain
 import mena.identity_presentation.generated.resources.bh_flag
 import mena.identity_presentation.generated.resources.dz_flag
 import mena.identity_presentation.generated.resources.eg_flag
+import mena.identity_presentation.generated.resources.egypt
 import mena.identity_presentation.generated.resources.iq_flag
 import mena.identity_presentation.generated.resources.ir_flag
+import mena.identity_presentation.generated.resources.iran
+import mena.identity_presentation.generated.resources.iraq
 import mena.identity_presentation.generated.resources.jo_flag
+import mena.identity_presentation.generated.resources.jordan
+import mena.identity_presentation.generated.resources.kuwait
 import mena.identity_presentation.generated.resources.kw_flag
 import mena.identity_presentation.generated.resources.lb_flag
+import mena.identity_presentation.generated.resources.lebanon
+import mena.identity_presentation.generated.resources.libya
 import mena.identity_presentation.generated.resources.ly_flag
 import mena.identity_presentation.generated.resources.ma_flag
+import mena.identity_presentation.generated.resources.morocco
 import mena.identity_presentation.generated.resources.om_flag
+import mena.identity_presentation.generated.resources.oman
+import mena.identity_presentation.generated.resources.palestine
 import mena.identity_presentation.generated.resources.ps_flag
 import mena.identity_presentation.generated.resources.qa_flag
+import mena.identity_presentation.generated.resources.qatar
 import mena.identity_presentation.generated.resources.sa_flag
+import mena.identity_presentation.generated.resources.saudi_arabia
+import mena.identity_presentation.generated.resources.sd_flag
+import mena.identity_presentation.generated.resources.so_flag
+import mena.identity_presentation.generated.resources.somalia
+import mena.identity_presentation.generated.resources.sudan
 import mena.identity_presentation.generated.resources.sy_flag
+import mena.identity_presentation.generated.resources.syria
 import mena.identity_presentation.generated.resources.tn_flag
+import mena.identity_presentation.generated.resources.tunisia
+import mena.identity_presentation.generated.resources.uae
 import mena.identity_presentation.generated.resources.ye_flag
+import mena.identity_presentation.generated.resources.yemen
 import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.StringResource
 
 enum class MenaCountries(
-    val countryName: String,
+    val countryNameRes: StringResource,
     val callingCode: String,
     val flagImage: DrawableResource
 ) {
-    ALGERIA("Algeria", "+213", Res.drawable.dz_flag),
-    BAHRAIN("Bahrain", "+973", Res.drawable.bh_flag),
-    EGYPT("Egypt", "+20", Res.drawable.eg_flag),
-    IRAN("Iran", "+98", Res.drawable.ir_flag),
-    IRAQ("Iraq", "+964", Res.drawable.iq_flag),
-    JORDAN("Jordan", "+962", Res.drawable.jo_flag),
-    KUWAIT("Kuwait", "+965", Res.drawable.kw_flag),
-    LEBANON("Lebanon", "+961", Res.drawable.lb_flag),
-    LIBYA("Libya", "+218", Res.drawable.ly_flag),
-    MOROCCO("Morocco", "+212", Res.drawable.ma_flag),
-    OMAN("Oman", "+968", Res.drawable.om_flag),
-    PALESTINE("Palestine", "+970", Res.drawable.ps_flag),
-    QATAR("Qatar", "+974", Res.drawable.qa_flag),
-    SAUDI_ARABIA("Saudi Arabia", "+966", Res.drawable.sa_flag),
-    SYRIA("Syria", "+963", Res.drawable.sy_flag),
-    TUNISIA("Tunisia", "+216", Res.drawable.tn_flag),
-    UAE("United Arab Emirates", "+971", Res.drawable.ae_flag),
-    YEMEN("Yemen", "+967", Res.drawable.ye_flag)
+
+    ALGERIA(Res.string.algeria, "+213", Res.drawable.dz_flag),
+    BAHRAIN(Res.string.bahrain, "+973", Res.drawable.bh_flag),
+    EGYPT(Res.string.egypt, "+20", Res.drawable.eg_flag),
+    IRAN(Res.string.iran, "+98", Res.drawable.ir_flag),
+    IRAQ(Res.string.iraq, "+964", Res.drawable.iq_flag),
+    JORDAN(Res.string.jordan, "+962", Res.drawable.jo_flag),
+    KUWAIT(Res.string.kuwait, "+965", Res.drawable.kw_flag),
+    LEBANON(Res.string.lebanon, "+961", Res.drawable.lb_flag),
+    LIBYA(Res.string.libya, "+218", Res.drawable.ly_flag),
+    MOROCCO(Res.string.morocco, "+212", Res.drawable.ma_flag),
+    OMAN(Res.string.oman, "+968", Res.drawable.om_flag),
+    PALESTINE(Res.string.palestine, "+970", Res.drawable.ps_flag),
+    QATAR(Res.string.qatar, "+974", Res.drawable.qa_flag),
+    SAUDI_ARABIA(Res.string.saudi_arabia, "+966", Res.drawable.sa_flag),
+    SOMALIA(Res.string.somalia, "+252", Res.drawable.so_flag),
+    SUDAN(Res.string.sudan, "+249", Res.drawable.sd_flag),
+    SYRIA(Res.string.syria, "+963", Res.drawable.sy_flag),
+    TUNISIA(Res.string.tunisia, "+216", Res.drawable.tn_flag),
+    UAE(Res.string.uae, "+971", Res.drawable.ae_flag),
+    YEMEN(Res.string.yemen, "+967", Res.drawable.ye_flag)
 }
