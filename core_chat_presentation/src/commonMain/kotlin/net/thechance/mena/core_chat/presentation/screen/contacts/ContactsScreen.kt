8f6c156cdf231af8c391ed6902e4739de72a86af
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-15 00:54:01 +0300

    replace ChatAppBar with AppBar component in Contacts and SyncContacts screens

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsScreen.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsScreen.kt
@@ -11,11 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import mena.core_chat_presentation.generated.resources.Res
 import mena.core_chat_presentation.generated.resources.contacts_title
+import mena.core_chat_presentation.generated.resources.ic_arrow_left
 import mena.core_chat_presentation.generated.resources.ic_resync
 import net.thechance.mena.core_chat.presentation.navigation.LocalNavController
 import net.thechance.mena.core_chat.presentation.navigation.SyncContactsRoute
 import net.thechance.mena.core_chat.presentation.screen.contacts.components.ContactsList
-import net.thechance.mena.core_chat.presentation.shared.components.ChatAppBar
+import net.thechance.mena.designsystem.presentation.component.appBar.AppBar
 import net.thechance.mena.designsystem.presentation.component.appBar.AppBarOptionContainer
 import net.thechance.mena.designsystem.presentation.component.icon.MenaIcon
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
@@ -40,19 +41,28 @@ fun ContactsScreen() {
 
 @Composable
 private fun ContactsContent(
-    modifier: Modifier = Modifier,
     onNavigateBack: () -> Unit,
     onResyncClick: () -> Unit,
     contacts: List<ContactUi>
 ) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize()
             .background(color = Theme.colorScheme.background.surface)
             .statusBarsPadding()
     ) {
-        ChatAppBar(
+        AppBar(
+            modifier = Modifier,
             title = stringResource(Res.string.contacts_title),
-            onNavigateBack = onNavigateBack,
+            contentPadding = PaddingValues(horizontal = 12.dp, vertical = 8.dp),
+            leadingContent = {
+                MenaIcon(
+                    painter = painterResource(Res.drawable.ic_arrow_left),
+                    modifier = Modifier.size(20.dp),
+                    contentDescription = null,
+                    tint = Theme.colorScheme.primary.primary,
+                )
+            },
+            onLeadingClick = onNavigateBack,
             trailingContent = {
                 AppBarOptionContainer(
                     badgeColor = Theme.colorScheme.primary.primary,
--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/syncContacts/SyncContactsScreen.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/syncContacts/SyncContactsScreen.kt
@@ -3,8 +3,10 @@ package net.thechance.mena.core_chat.presentation.screen.syncContacts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -15,15 +17,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import mena.core_chat_presentation.generated.resources.Res
+import mena.core_chat_presentation.generated.resources.ic_arrow_left
 import mena.core_chat_presentation.generated.resources.sync_contacts
 import net.thechance.mena.core_chat.presentation.navigation.LocalNavController
 import net.thechance.mena.core_chat.presentation.screen.syncContacts.components.ContactsSyncedView
 import net.thechance.mena.core_chat.presentation.screen.syncContacts.components.NoContactsSyncView
 import net.thechance.mena.core_chat.presentation.screen.syncContacts.components.PhoneIcon
-import net.thechance.mena.core_chat.presentation.shared.components.ChatAppBar
+import net.thechance.mena.designsystem.presentation.component.appBar.AppBar
+import net.thechance.mena.designsystem.presentation.component.icon.MenaIcon
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -44,19 +50,28 @@ fun SyncContactsScreen(
 private fun SyncContactsContent(
     isSyncing: Boolean,
     onSyncClick: () -> Unit,
-    modifier: Modifier = Modifier,
 ) {
     val navController = LocalNavController.current
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize()
             .background(color = Theme.colorScheme.background.surface)
             .statusBarsPadding()
     ) {
-        ChatAppBar(
-            onNavigateBack = {
+        AppBar(
+            modifier = Modifier,
+            title = stringResource(Res.string.sync_contacts),
+            contentPadding = PaddingValues(horizontal = 12.dp, vertical = 8.dp),
+            leadingContent = {
+                MenaIcon(
+                    painter = painterResource(Res.drawable.ic_arrow_left),
+                    modifier = Modifier.size(20.dp),
+                    contentDescription = null,
+                    tint = Theme.colorScheme.primary.primary,
+                )
+            },
+            onLeadingClick = {
                 navController.popBackStack()
             },
-            title = stringResource(Res.string.sync_contacts)
         )
         Column(
             modifier = Modifier
