16005dbd6b570b873be8f2abeea6af801bfa8808
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-15 00:59:02 +0300

    remove unused DummyContactsDataSource from dataProviderModule

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/dataProviderModule.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/dataProviderModule.kt
@@ -1,13 +1,11 @@
 package net.thechance.mena.core_chat.data.di
 
 import com.bilalazzam.contacts_provider.ContactsProvider
-import net.thechance.mena.core_chat.data.contacts.source.remote.DummyContactsDataSource
 import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
 internal val dataProviderModule = module {
     single { createContactsProvider() }
-    single { DummyContactsDataSource() }
 }
 
 expect fun Scope.createContactsProvider(): ContactsProvider
