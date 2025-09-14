e6416e6636700dc05f1d325a6f07f3d629d9b4d0
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-09-14 16:55:33 +0100

    feature: add CountrySelectableRowItem component

---
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/components/CountrySelectableRowItem.kt
@@ -0,0 +1,101 @@
+package net.thechance.mena.identity.presentation.components
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import net.thechance.mena.designsystem.presentation.component.button.radioButton.RadioButton
+import net.thechance.mena.designsystem.presentation.component.text.MenaText
+import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import net.thechance.mena.identity.presentation.countryPicker.menaCountries.MenaCountries
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+internal fun CountrySelectableRowItem(
+    selectedCountry: MenaCountries,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: (MenaCountries) -> Unit = {}
+) {
+
+    val animatedCountryItemColor by animateColorAsState(
+        targetValue = if (isSelected) Theme.colorScheme.background.surfaceHigh
+        else Theme.colorScheme.background.surfaceLow
+    )
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = animatedCountryItemColor,
+                shape = RoundedCornerShape(Theme.radius.lg)
+            )
+            .clip(RoundedCornerShape(Theme.radius.lg))
+            .clickable(
+                onClick = {
+                    onClick(selectedCountry)
+                },
+                indication = ripple(),
+                interactionSource = remember { MutableInteractionSource() }
+            )
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+    ) {
+
+        FlagImage(painterResource(selectedCountry.flagImage))
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(2.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)
+        ) {
+            MenaText(
+                text = stringResource(selectedCountry.countryNameRes),
+                color = Theme.colorScheme.primary.primary,
+                style = Theme.typography.title.small,
+            )
+
+            MenaText(
+                text = "(${selectedCountry.callingCode})",
+                color = Theme.colorScheme.shadeSecondary,
+                style = Theme.typography.label.small,
+            )
+        }
+
+        RadioButton(
+            isSelected = isSelected,
+            onClick = null
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CountrySelectableRowItemPreview() {
+    MenaTheme {
+        CountrySelectableRowItem(
+            selectedCountry = MenaCountries.PALESTINE,
+            isSelected = true,
+            onClick = {}
+        )
+    }
+
+}
