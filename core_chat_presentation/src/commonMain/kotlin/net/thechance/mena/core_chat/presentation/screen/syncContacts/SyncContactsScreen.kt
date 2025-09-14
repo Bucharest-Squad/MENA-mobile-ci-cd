b63fe3399a89692de74deb61547a3604fcaa69db
Mona <monaayman584@gmail.com>
2025-09-15 00:39:12 +0300

    chore: remove unneeded import

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/syncContacts/SyncContactsScreen.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/syncContacts/SyncContactsScreen.kt
@@ -3,7 +3,6 @@ package net.thechance.mena.core_chat.presentation.screen.syncContacts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
