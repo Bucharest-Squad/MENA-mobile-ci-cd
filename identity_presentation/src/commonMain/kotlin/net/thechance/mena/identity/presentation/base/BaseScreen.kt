fe50bb6d6e60e32fb7d2217be223fc9eb13df781
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-09-14 17:03:10 +0300

    refactor update `collectAsState` to `collectAsStateWithLifecycle`

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/base/BaseScreen.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/base/BaseScreen.kt
@@ -2,8 +2,8 @@ package net.thechance.mena.identity.presentation.base
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
@@ -21,7 +21,7 @@ abstract class BaseScreen<VM, S, E, I> : Screen
         where  VM : BaseScreenModel<S, E>, I : BaseInteractionListener, VM : I {
     @Composable
     fun InitScreen(viewModel: VM) {
-        val state: S by viewModel.state.collectAsState()
+        val state: S by viewModel.state.collectAsStateWithLifecycle()
         val navigator = LocalNavigator.currentOrThrow
 
         OnRender(state, viewModel)
