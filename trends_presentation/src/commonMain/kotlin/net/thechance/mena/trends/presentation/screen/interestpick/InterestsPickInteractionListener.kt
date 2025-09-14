2a35758b24359865fd41907e82b1b840cebcf68a
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-09-14 17:47:36 +0300

    add InterestsPickInteractionListener

---
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/interestpick/InterestsPickInteractionListener.kt
@@ -0,0 +1,7 @@
+package net.thechance.mena.trends.presentation.screen.interestpick
+
+interface InterestsPickInteractionListener {
+    fun onCategoryClick(categoryId: Int)
+    fun onSaveClick()
+    fun onBackClick()
+}
