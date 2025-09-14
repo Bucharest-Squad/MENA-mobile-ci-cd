69333c6784748c2e0e4e5a5213ef3838a2539471
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-15 00:46:07 +0300

    remove unused Koin modules and clean up App.kt

--- composeApp/src/commonMain/kotlin/net/thechance/mena/App.kt
+++ composeApp/src/commonMain/kotlin/net/thechance/mena/App.kt
@@ -1,29 +1,10 @@
 package net.thechance.mena
 
 import androidx.compose.runtime.Composable
-import net.thechance.mena.core_chat.api.CoreChatApi
-import net.thechance.mena.core_chat.data.di.chatDataModule
-import net.thechance.mena.core_chat.presentation.di.chatPresentationModule
-import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.KoinApplication
-import org.koin.compose.getKoin
 
 @Composable
 @Preview
 fun App() {
-    KoinApplication(
-        application = {
-            modules(
-                chatPresentationModule,
-                chatPresentationModule,
-                chatDataModule,
-            )
-        }
-    ) {
-        MenaTheme {
-            val coreChatApi: CoreChatApi = getKoin().get()
-            coreChatApi.launch()
-        }
-    }
+
 }
