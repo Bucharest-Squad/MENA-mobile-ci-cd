a2f0b3748672899adba8a5de43500634b4a4b4b7
Aboud <gh.abd.com.207@gmail.com>
2025-09-15 09:13:42 -0700

    add deploy for develop , production , staging

--- composeApp/src/commonMain/kotlin/net/thechance/mena/App.kt
+++ composeApp/src/commonMain/kotlin/net/thechance/mena/App.kt
@@ -24,6 +24,7 @@ import mena.composeapp.generated.resources.compose_multiplatform
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        let have an error here to test the notification
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
