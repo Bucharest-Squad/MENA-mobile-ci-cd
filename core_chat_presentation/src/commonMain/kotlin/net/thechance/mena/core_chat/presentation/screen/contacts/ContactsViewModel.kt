ab19ef1c0e264d3870db4dcdd78d15bfd0070efd
Radwa85 <radwa.mohamed5033@gmail.com>
2025-09-15 15:38:29 +0300

    implement getContacts in ContactsViewModel

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsViewModel.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsViewModel.kt
@@ -1,8 +1,30 @@
+
 package net.thechance.mena.core_chat.presentation.screen.contacts
 
-import androidx.lifecycle.ViewModel
+import net.thechance.mena.core_chat.domain.entity.Contact
 import net.thechance.mena.core_chat.domain.repository.ContactsRepository
+import net.thechance.mena.core_chat.presentation.shared.BasePagingSource
+import net.thechance.mena.core_chat.presentation.shared.BaseViewModel
 
-class ContactsViewModel(private val contactsRepository: ContactsRepository): ViewModel() {
+class ContactsViewModel(
+    private val contactsRepository: ContactsRepository
+) : BaseViewModel<ContactsUiState>(ContactsUiState()) {
 
+    init {
+        getContacts()
+    }
+
+    fun getContacts() {
+        val contactsFlow = createPagingFlow(
+            pagingSourceFactory = {
+                BasePagingSource(
+                    onError = { throwable -> updateState { it.copy(error = throwable.message) } },
+                    fetchItems = { page, pageSize -> contactsRepository.getUserContacts(page, pageSize) }
+                )
+            },
+            mapper = { contact: Contact -> contact.toUiModel() }
+        )
+        updateState { it.copy(contacts = contactsFlow) }
+    }
 }
+
