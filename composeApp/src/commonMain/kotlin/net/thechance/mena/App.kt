541ac36ce4d0488ba4261f9ddf5efd012fcd32d2
Radwa85 <radwa.mohamed5033@gmail.com>
2025-09-15 15:43:02 +0300

    remove chatNavHost and permission from manifest

--- composeApp/src/commonMain/kotlin/net/thechance/mena/App.kt
+++ composeApp/src/commonMain/kotlin/net/thechance/mena/App.kt
@@ -1,14 +1,11 @@
 package net.thechance.mena
 
 import androidx.compose.runtime.Composable
-import net.thechance.mena.core_chat.presentation.navigation.ChatNavHost
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MenaTheme {
-        ChatNavHost()
-    }
+    MenaTheme {}
 }
