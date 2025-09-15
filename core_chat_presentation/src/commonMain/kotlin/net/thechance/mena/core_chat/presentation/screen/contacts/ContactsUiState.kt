71ec0bb445ba0507f70f7740fc3f0d4a05807bcc
Radwa85 <radwa.mohamed5033@gmail.com>
2025-09-15 15:38:02 +0300

    create contact screen ui state

---
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsUiState.kt
@@ -0,0 +1,12 @@
+package net.thechance.mena.core_chat.presentation.screen.contacts
+
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class ContactsUiState(
+    val contacts: Flow<PagingData<ContactUiModel>> = emptyFlow(),
+    val isPagingLoading: Boolean = false,
+    val isLastPage: Boolean = false,
+    val error: String? = null
+)   
