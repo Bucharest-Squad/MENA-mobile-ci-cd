0114dd261ee2e401a776637d5b93fd0fd9e47ff7
norhanadel <norhanadel367@gmail.com>
2025-09-16 18:25:57 +0300

    change some request in vm and screen

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/managemytrendscreen/ManageTrendsViewModel.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/managemytrendscreen/ManageTrendsViewModel.kt
@@ -35,7 +35,6 @@ class ManageTrendsViewModel(
 
     override fun onReelItemClick(reelId: Int) {
         sendEffect(ManageTrendsUiEffect.NavigateToTrend(reelId))
-
     }
 
     override fun onBackClick() {
