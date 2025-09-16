6c210439da378eb2fcaad56795ef9b2f00ce7e46
haider <haideraljawasim@gmail.com>
2025-09-16 16:20:47 +0300

    update kotlin serlizetion plugin

--- dukan_presentation/build.gradle.kts
+++ dukan_presentation/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 kotlin {
