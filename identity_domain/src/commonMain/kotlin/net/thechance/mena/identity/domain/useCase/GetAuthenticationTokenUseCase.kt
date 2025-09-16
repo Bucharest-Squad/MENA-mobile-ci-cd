eaead6aa1642661196af2a0c04151da29ecd28f0
pola george <86931053+poula22@users.noreply.github.com>
2025-09-16 11:59:43 +0300

    refactor: remove bridge class GetAuthenticationTokenUseCase

--- identity_domain/src/commonMain/kotlin/net/thechance/mena/identity/domain/useCase/GetAuthenticationTokenUseCase.kt
+++
@@ -1,9 +0,0 @@
-package net.thechance.mena.identity.domain.useCase
-
-import net.thechance.mena.identity.domain.repository.AuthenticationRepository
-
-class GetAuthenticationTokenUseCase(
-    private val authenticationRepository: AuthenticationRepository
-) {
-    suspend operator fun invoke() = authenticationRepository.getToken()
-}
