db1934f9a7a2e7ee9170add2e185099ba3e719fe
Hasan Ali <92353024+Hasa9nAli@users.noreply.github.com>
2025-09-17 00:14:39 +0300

    Feature/FTH-18 empty book mark (#53)
    * FTH18: add resource directory for commonMain source set
    
    * FTH-18: add resource directory for commonMain source set
    
    * FTH-18: add vector asset for unsaved bookmark and string resource for tilawah
    
    * FTH-18: add EmptyBookMarkState composable for displaying empty bookmark state
    
    * FTH-18: center-align text in EmptyBookMarkState composable
    
    * FTH-18: rename EmptyBookMarkState to EmptyBookmarkState and simplify padding

--- faith_presentation/build.gradle.kts
+++ faith_presentation/build.gradle.kts
@@ -51,6 +51,8 @@ kotlin {
 android {
     namespace = "net.thechance.mena.faith.presentation"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
+    
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
