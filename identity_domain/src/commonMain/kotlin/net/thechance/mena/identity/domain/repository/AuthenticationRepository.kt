9c37bb0bfa2e67d39b7891bfcfffe24fa6c8c904
pola george <86931053+poula22@users.noreply.github.com>
2025-09-17 17:51:57 +0300

    refactor: remove unused function isLoggedIn

--- identity_domain/src/commonMain/kotlin/net/thechance/mena/identity/domain/repository/AuthenticationRepository.kt
+++ identity_domain/src/commonMain/kotlin/net/thechance/mena/identity/domain/repository/AuthenticationRepository.kt
@@ -2,7 +2,6 @@ package net.thechance.mena.identity.domain.repository
 
 interface AuthenticationRepository {
     suspend fun login(countryCode: String, number: String, password: String)
-    suspend fun isLoggedIn(): Boolean
     suspend fun isUserBlocked(countryCode: String, number: String): Boolean
     suspend fun getToken(): String
 }
