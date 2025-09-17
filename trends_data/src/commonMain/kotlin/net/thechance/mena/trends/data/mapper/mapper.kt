a7a5ea033b8e33d2ca2d8abf794573be4ed5bc0d
Hend Sayed <hendsayed853@gmail.com>
2025-09-17 23:04:12 +0300

    add mappers for CategoryDto and ReelDto to entity

---
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/mapper/mapper.kt
@@ -0,0 +1,26 @@
+package net.thechance.mena.trends.data.mapper
+
+import kotlinx.datetime.LocalDateTime
+import net.thechance.mena.trends.data.dto.CategoryDto
+import net.thechance.mena.trends.data.dto.ReelDto
+import net.thechance.mena.trends.domain.entity.Category
+import net.thechance.mena.trends.domain.entity.Reel
+
+fun CategoryDto.toEntity() =
+    Category(
+        id = id,
+        name = name,
+        emoji = iconPath
+    )
+
+fun ReelDto.toEntity() =
+    Reel(
+        id = id,
+        thumbnailUrl = reelImage,
+        videoUrl = reelUrl,
+        description = description,
+        likesCount = likesCount,
+        viewsCount = viewsCount,
+        createdAt = LocalDateTime.parse(createdAt),
+        categories = categories.map { it.toEntity() }
+    )
