4b60dd8cdbc2de4a8e1f1bd5725eb03763d38d51
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-09-18 01:58:53 +0300

    update test dependencies and improve formatting in ContactsViewModelTest

--- core_chat_presentation/src/test/java/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsViewModelTest.kt
+++ core_chat_presentation/src/test/java/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsViewModelTest.kt
@@ -5,8 +5,8 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import net.thechance.mena.core_chat.domain.repository.ContactsRepository
-import org.junit.Assert.assertNotNull
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class ContactsViewModelTest {
@@ -29,7 +29,7 @@ class ContactsViewModelTest {
 
 
     @Test
-    fun `onRefreshContacts handles empty contact list`()=runTest {
+    fun `onRefreshContacts handles empty contact list`() = runTest {
         coEvery { contactsRepository.getUserContacts(any(), any()) } returns mockk()
         viewModel.onRefreshContacts()
         viewModel.state.test {
@@ -40,7 +40,7 @@ class ContactsViewModelTest {
     }
 
     @Test
-    fun `onRefreshContacts handles data load error`() =runTest{
+    fun `onRefreshContacts handles data load error`() = runTest {
         coEvery { contactsRepository.getUserContacts(any(), any()) } throws Exception()
         viewModel.onRefreshContacts()
         viewModel.state.test {
@@ -59,16 +59,16 @@ class ContactsViewModelTest {
     }
 
     @Test
-    fun `onResyncClick emits NavigateToSyncContacts effect`() =runTest{
-       viewModel.effect.test {
-           viewModel.onResyncClick()
-           val item = awaitItem()
-          assertEquals(ContactsScreenEffect.NavigateToSyncContacts, item)
-       }
+    fun `onResyncClick emits NavigateToSyncContacts effect`() = runTest {
+        viewModel.effect.test {
+            viewModel.onResyncClick()
+            val item = awaitItem()
+            assertEquals(ContactsScreenEffect.NavigateToSyncContacts, item)
+        }
     }
 
     @Test
-    fun `onContactClick emits NavigateToChatScreen effect with valid contactId`()=runTest {
+    fun `onContactClick emits NavigateToChatScreen effect with valid contactId`() = runTest {
         viewModel.effect.test {
             viewModel.onContactClick(1)
             val item = awaitItem()
