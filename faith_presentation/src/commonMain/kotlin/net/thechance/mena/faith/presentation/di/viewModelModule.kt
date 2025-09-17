8f153995a0f019feac6c0a2c1aa56c4307d05332
Bassant Mohammed <bassant.22230@gmail.com>
2025-09-17 19:52:52 +0300

    chore(merge): sync develop into read quran story

--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/di/viewModelModule.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/di/viewModelModule.kt
@@ -1,9 +1,7 @@
 package net.thechance.mena.faith.presentation.di
 
-import net.thechance.mena.faith.presentation.feature.quran.sur.SurViewModel
-import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 internal val viewModelModule = module {
-    viewModelOf(::SurViewModel)
+
 }
