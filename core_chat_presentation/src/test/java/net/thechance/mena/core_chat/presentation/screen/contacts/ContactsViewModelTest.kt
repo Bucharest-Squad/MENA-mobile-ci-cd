cbac0c27cb10bd075b0249685e64373549f207a9
Mona <monaayman584@gmail.com>
2025-09-18 12:28:57 +0300

    test: add onRefreshContacts

--- core_chat_presentation/src/test/java/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsViewModelTest.kt
+++ core_chat_presentation/src/test/java/net/thechance/mena/core_chat/presentation/screen/contacts/ContactsViewModelTest.kt
@@ -3,7 +3,10 @@ package net.thechance.mena.core_chat.presentation.screen.contacts
 import app.cash.turbine.test
 import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import net.thechance.mena.core_chat.domain.entity.Contact
+import net.thechance.mena.core_chat.domain.model.PagedData
 import net.thechance.mena.core_chat.domain.repository.ContactsRepository
 import org.junit.Assert.assertNotNull
 import org.junit.Test
@@ -19,17 +22,31 @@ class ContactsViewModelTest {
     }
 
     @Test
-    fun `onRefreshContacts loads contacts`() = runTest {
-        coEvery { contactsRepository.getUserContacts(any(), any()) } returns mockk()
+    fun `onRefreshContacts loads expected contact`() = runTest {
+        val expectedContact = Contact(
+            firstName = "Ali",
+            lastName = "Ahmed",
+            phone = "010",
+            isMenaUser = true,
+            imageUrl = null
+        )
+
+        coEvery { contactsRepository.getUserContacts(any(), any()) } returns PagedData(
+            data = listOf(expectedContact),
+            totalItems = 1,
+            isLastPage = true
+        )
+
         viewModel.onRefreshContacts()
         viewModel.state.test {
-            val item = awaitItem()
+            val state = awaitItem()
+            assertEquals<Any>(expectedContact, state.contacts.first())
+            cancelAndIgnoreRemainingEvents()
         }
     }
 
-
     @Test
-    fun `onRefreshContacts handles empty contact list`()=runTest {
+    fun `onRefreshContacts handles empty contact list`() = runTest {
         coEvery { contactsRepository.getUserContacts(any(), any()) } returns mockk()
         viewModel.onRefreshContacts()
         viewModel.state.test {
@@ -40,7 +57,7 @@ class ContactsViewModelTest {
     }
 
     @Test
-    fun `onRefreshContacts handles data load error`() =runTest{
+    fun `onRefreshContacts handles data load error`() = runTest {
         coEvery { contactsRepository.getUserContacts(any(), any()) } throws Exception()
         viewModel.onRefreshContacts()
         viewModel.state.test {
@@ -59,16 +76,16 @@ class ContactsViewModelTest {
     }
 
     @Test
-    fun `onResyncClick emits NavigateToSyncContacts effect`() =runTest{
-       viewModel.effect.test {
-           viewModel.onResyncClick()
-           val item = awaitItem()
-          assertEquals(ContactsScreenEffect.NavigateToSyncContacts, item)
-       }
+    fun `onResyncClick emits NavigateToSyncContacts effect`() = runTest {
+        viewModel.effect.test {
+            viewModel.onResyncClick()
+            val item = awaitItem()
+            assertEquals(ContactsScreenEffect.NavigateToSyncContacts, item)
+        }
     }
 
     @Test
-    fun `onContactClick emits NavigateToChatScreen effect with valid contactId`()=runTest {
+    fun `onContactClick emits NavigateToChatScreen effect with valid contactId`() = runTest {
         viewModel.effect.test {
             viewModel.onContactClick(1)
             val item = awaitItem()
