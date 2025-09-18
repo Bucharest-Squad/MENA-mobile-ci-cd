88bca3166701dcbbdfeafb6820eb52d649cb0cfa
Zyad <zyadhammad531@gmail.com>
2025-09-18 08:46:16 +0300

    feat: start exception mapping from data to domain

--- trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/safeApiCall.kt
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/safeApiCall.kt
@@ -1,17 +1,53 @@
 package net.thechance.mena.trends.data.util
 
-suspend inline fun <T> safeApiCall(
-    execute: suspend () -> T
+import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpStatusCode
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.io.IOException
+import net.thechance.mena.trends.domain.exception.NoInternetException
+
+suspend inline fun <reified T> safeApiCall(
+    execute: suspend () -> HttpResponse
 ): T {
     val result = try {
         execute()
-    } catch (e: Exception) {
-        throw e // TODO: handle exceptions after adding domain exceptions
+    } catch (_: IOException) {
+        throw NoInternetException()
+    } catch (_: UnresolvedAddressException) {
+        throw NoInternetException()
+    } catch (exception: Exception) {
+        throw exception
     }
 
     return handleErrorStatus(result)
 }
 
-fun <T> handleErrorStatus(result: T): T {
-    return result // TODO: map error status numbers to exceptions after adding domain exceptions
+suspend inline fun <reified T> handleErrorStatus(result: HttpResponse): T {
+    TODO()
+//    return when (result.status.value) {
+//        in 200..299 -> {
+//            result.body<T>()
+//        }
+//
+//        in 400..499 -> {
+//            when (result.status) {
+//                HttpStatusCode.Unauthorized -> {
+//                    TODO()
+//                }
+//                HttpStatusCode.NotFound -> {
+//                    TODO()
+//                }
+//                else ->
+//            }
+//        }
+//
+//        in 500..599 -> {
+//            TODO()
+//        }
+//        else -> {
+//            TODO()
+//        }
+//    }
 }
