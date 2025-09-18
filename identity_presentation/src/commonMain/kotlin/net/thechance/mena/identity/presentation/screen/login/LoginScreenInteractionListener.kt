8a652c9e87655ee23fdfe30599d17d54f6e45e02
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-09-18 17:18:11 +0100

    feature: add country picker interactions to login

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/login/LoginScreenInteractionListener.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/login/LoginScreenInteractionListener.kt
@@ -1,6 +1,12 @@
 package net.thechance.mena.identity.presentation.screen.login
 
 import net.thechance.mena.identity.presentation.base.BaseInteractionListener
+import net.thechance.mena.identity.presentation.countryPicker.menaCountries.MenaCountries
 
 interface LoginScreenInteractionListener : BaseInteractionListener {
+
+    fun onClickCountryPicker()
+    fun onSelectCountryItem(country: MenaCountries)
+    fun onClickConfirmButton()
+    fun onDismissBottomSheet()
 }
