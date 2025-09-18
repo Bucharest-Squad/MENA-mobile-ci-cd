d8e1e7c6d59bfea385615eea4ba2ac5b6758cf0f
Aboud <gh.abd.com.207@gmail.com>
2025-09-18 09:13:41 -0700

    Add PR open Slack notification workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    sadfsadf
+    sadfsadfasd
 }
 
 tasks.register("exportModuleDeps") {
