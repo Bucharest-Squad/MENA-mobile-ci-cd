0cce7ce93d9b0536f807495f8465c3da63ef511a
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-09-19 14:00:21 +0300

    remove changes on main activity file

--- composeApp/src/androidMain/kotlin/net/thechance/mena/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/net/thechance/mena/MainActivity.kt
@@ -1,13 +1,11 @@
 package net.thechance.mena
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,14 +22,4 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppAndroidPreview() {
     App()
-}
-
-class ChatApp: Application(){
-
-    override fun onCreate() {
-        super.onCreate()
-        initKoin{
-            androidContext(this@ChatApp)
-        }
-    }
 }
