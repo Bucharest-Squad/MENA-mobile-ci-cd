a02af04db019a9211dac84d0c3ac3e35f65edbc8
Zyad <zyadhammad531@gmail.com>
2025-09-19 23:46:34 +0300

    feat: make a util for mapping error states into string resource

---
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/base/errorStateToStringResource.kt
@@ -0,0 +1,13 @@
+package net.thechance.mena.trends.presentation.shared.base
+
+import mena.trends_presentation.generated.resources.Res
+import mena.trends_presentation.generated.resources.error_generic
+import mena.trends_presentation.generated.resources.error_no_internet
+import org.jetbrains.compose.resources.StringResource
+
+internal fun ErrorState.toStringResource(): StringResource {
+    return when (this) {
+        ErrorState.NoInternet -> Res.string.error_no_internet
+        is ErrorState.RequestFailed -> Res.string.error_generic
+    }
+}
