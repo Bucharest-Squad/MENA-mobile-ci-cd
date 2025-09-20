1fd6bec8675f7e55edb0a2416809255b2de35561
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-09-20 22:28:01 +0100

    fix: solve merge conflicts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,6 +37,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.android)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -47,6 +48,10 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(projects.designSystem)
+            implementation(libs.bundles.voyager)
+            implementation(projects.identityPresentation)
+            implementation(projects.identityData)
+            implementation(projects.identityDomain)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
