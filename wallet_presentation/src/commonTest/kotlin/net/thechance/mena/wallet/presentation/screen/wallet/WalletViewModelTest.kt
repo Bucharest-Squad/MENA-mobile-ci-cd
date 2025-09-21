07ac43a4558ee6fce69ce7f26827dd6f3571fb8f
edrees <muhammed.edrees101@gmail.com>
2025-09-21 03:54:40 +0300

    test: ensure WalletViewModel state initializes correctly with loading state

--- wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/wallet/WalletViewModelTest.kt
+++ wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/wallet/WalletViewModelTest.kt
@@ -49,6 +49,7 @@ class WalletViewModelTest {
         }
 
         val viewModel = WalletViewModel(balanceRepository)
+        advanceUntilIdle()
 
         viewModel.state.test {
             val initialState = awaitItem()
