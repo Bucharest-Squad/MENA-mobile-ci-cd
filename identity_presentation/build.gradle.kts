05da315ee9dee74f7d7363633f13eb8fd515bfb7
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-09-22 13:07:29 +0300

    fix gradle issues after last merge

--- identity_presentation/build.gradle.kts
+++ identity_presentation/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.devMokkery)
+    alias(libs.plugins.mockkery)
     alias(libs.plugins.kover)
 
 }
