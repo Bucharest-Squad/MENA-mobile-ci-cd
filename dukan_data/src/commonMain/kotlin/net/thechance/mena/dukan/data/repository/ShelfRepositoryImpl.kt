72205a91d01282a87b2bcdf57c7d20e470ffc8d6
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-24 22:03:34 +0300

    feat: implement create shelf screen

---
+++ dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/repository/ShelfRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package net.thechance.mena.dukan.data.repository
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import net.thechance.mena.dukan.data.repository.mapper.toCreateShelfRequest
+import net.thechance.mena.dukan.data.repository.util.safeApiCall
+import net.thechance.mena.dukan.domain.entity.Shelf
+import net.thechance.mena.dukan.domain.repository.ShelfRepository
+
+class ShelfRepositoryImpl(
+    private val client: HttpClient
+) : ShelfRepository {
+
+    override suspend fun createShelf(shelf: Shelf) {
+        safeApiCall<Unit> {
+            client.post("$BASE_URL/shelf/create") {
+                contentType(ContentType.Application.Json)
+                setBody(shelf.toCreateShelfRequest())
+            }
+        }
+    }
+
+    override suspend fun getMyDukanShelves(): List<Shelf> {
+        TODO("Provide the return value")
+    }
+
+    override suspend fun deleteShelf(shelfId: String): Boolean {
+        TODO("Provide the return value")
+    }
+
+    companion object {
+        private const val BASE_URL = "/dukan"
+    }
+}
