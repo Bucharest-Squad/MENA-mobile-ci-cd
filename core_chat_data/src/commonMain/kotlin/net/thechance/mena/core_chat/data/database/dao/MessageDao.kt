75ddda75676b2932a05ede9ef3a4159a8a006ae1
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-09-24 19:03:23 +0300

    add ChatDa with necessary queries and converters and move MessageDao to Dao package.

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/database/dao/MessageDao.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/database/dao/MessageDao.kt
@@ -1,11 +1,11 @@
-package net.thechance.mena.core_chat.data.messages.dao
+package net.thechance.mena.core_chat.data.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
-import net.thechance.mena.core_chat.data.messages.MessageEntity
-import net.thechance.mena.core_chat.data.messages.MessageStatus
+import net.thechance.mena.core_chat.data.database.entity.MessageEntity
+import net.thechance.mena.core_chat.data.database.entity.MessageStatus
 
 @Dao
 interface MessageDao {
@@ -15,9 +15,9 @@ interface MessageDao {
     @Query("SELECT * FROM MessageEntity ORDER BY timestamp ASC")
     fun getAllMessages(): Flow<List<MessageEntity>>
 
-    @Query("SELECT * FROM MessageEntity WHERE chatId = :chatId ORDER BY timestamp ASC")
+    @Query("SELECT * FROM MessageEntity WHERE chat_id = :chatId ORDER BY timestamp ASC")
     fun getMessagesByChat(chatId: Long): Flow<List<MessageEntity>>
 
-    @Query("UPDATE MessageEntity SET status = :status WHERE id = :id")
+    @Query("UPDATE MessageEntity SET message_status = :status WHERE id = :id")
     suspend fun updateMessageStatus(id: Long, status: MessageStatus)
 }
