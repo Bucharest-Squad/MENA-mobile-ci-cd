97725256ecc936183864d258a18695e97755f8e4
bilalazzam <blalazam065@gmail.com>
2025-09-24 07:45:23 +0300

    add chat ui state

---
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/messaging/ChatUiState.kt
@@ -0,0 +1,7 @@
+package net.thechance.mena.core_chat.presentation.screen.messaging
+
+data class ChatUiState(
+    val id: String = "",
+    val name: String = "",
+    val avatarUrl: String = ""
+)
