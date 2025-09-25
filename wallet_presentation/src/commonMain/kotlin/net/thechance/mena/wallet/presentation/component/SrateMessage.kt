d5fc4d32d6502822966de6ff2f3425efa0e859fb
Gitex <gehadepada@gmail.com>
2025-09-25 20:46:21 +0300

    feat:implement StateMessage component

---
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/SrateMessage.kt
@@ -0,0 +1,74 @@
+package net.thechance.mena.wallet.presentation.component
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import mena.wallet_presentation.generated.resources.Res
+import mena.wallet_presentation.generated.resources.img_filter_error
+import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StateMessage(
+    image: Painter,
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+    imageModifier: Modifier = Modifier,
+    horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Center,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = horizontalAlignment,
+        verticalArrangement = verticalArrangement
+    ) {
+        Image(
+            modifier = imageModifier
+                .padding(bottom = 16.dp)
+                .size(width = 112.dp, height = 115.dp),
+            painter = image,
+            contentDescription = title
+        )
+        Text(
+            modifier = Modifier.padding(bottom = 8.dp),
+            text = title,
+            color = Theme.colorScheme.shadePrimary,
+            style = Theme.typography.title.small,
+        )
+        Text(
+            text = description,
+            color = Theme.colorScheme.shadeSecondary,
+            style = Theme.typography.body.small,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun StateMessagePreview() {
+    MenaTheme{
+            StateMessage(
+                image = painterResource(Res.drawable.img_filter_error),
+                title = "Error Title",
+                description = "This is a detailed description of the error."
+            )
+        }
+}
