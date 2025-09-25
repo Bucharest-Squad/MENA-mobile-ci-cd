0edc2e9454615993cebf51a70a3b483e58ececd5
Malak Raef <malak.raef20@gmail.com>
2025-09-25 18:18:14 +0300

    remove material3 dependency from wallet.presentation

--- wallet_presentation/build.gradle.kts
+++ wallet_presentation/build.gradle.kts
@@ -39,7 +39,6 @@ kotlin {
             implementation(projects.designSystem)
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
