caf863017603492dda71da99b9a0a3e591bc498b
Malak Raef <malak.raef20@gmail.com>
2025-09-25 18:59:10 +0300

    refactor: refactor the date picker design so it becomes responsive and adaptive for different screen and font sizes

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/filter/datePicker/WheelDatePicker.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/filter/datePicker/WheelDatePicker.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,6 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.PagerSnapDistance
@@ -23,12 +27,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,44 +70,44 @@ fun WheelDatePicker(
         }
     }
 
+    var rowWidth by remember { mutableStateOf(0) }
+
+
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(200.dp)
+            .height(200.dp),
+        contentAlignment = Alignment.Center
     ) {
 
         ChoiceIndicator(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 60.dp)
-                .align(Alignment.Center)
+                .width(with(LocalDensity.current) { rowWidth.toDp() })
+                .fillMaxHeight()
         )
+
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .onSizeChanged { rowWidth = it.width }
+                .align(Alignment.Center),
+            horizontalArrangement = Arrangement.spacedBy(27.dp, Alignment.CenterHorizontally),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Spacer(modifier = Modifier.weight(15f))
-
             VerticalPicker(
                 state = dayPagerState,
-                options = days,
-                modifier = Modifier.weight(11f).fillMaxHeight()
+                options = days
             )
 
             VerticalPicker(
                 state = monthPagerState,
-                options = (1..12).map { getMonthName(it) },
-                modifier = Modifier.weight(29f).fillMaxHeight()
+                options = (1..12).map { getMonthName(it) }
             )
 
             VerticalPicker(
                 state = yearPagerState,
-                options = (MIN_YEAR..MAX_YEAR).map { it.toString() },
-                modifier = Modifier.weight(11f).fillMaxHeight(),
+                options = (MIN_YEAR..MAX_YEAR).map { it.toString() }
             )
 
-            Spacer(modifier = Modifier.weight(15f))
         }
     }
 }
@@ -138,11 +147,26 @@ private fun VerticalPicker(
     options: List<String>,
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier = modifier) {
+    val textMeasurer = rememberTextMeasurer()
+    val baseStyle = Theme.typography.body.large
 
+    val maxWidth = remember(options) {
+        options.maxOf { option ->
+            textMeasurer.measure(
+                text = option,
+                style = baseStyle
+            ).size.width
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .width(with(LocalDensity.current) { maxWidth.toDp() })
+            .fillMaxHeight()
+    ) {
         VerticalPager(
             state = state,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxHeight().wrapContentWidth(),
             beyondViewportPageCount = 5,
             horizontalAlignment = Alignment.CenterHorizontally,
             pageSpacing = 4.dp,
@@ -154,7 +178,6 @@ private fun VerticalPicker(
                 snapAnimationSpec = tween(durationMillis = 200, easing = LinearOutSlowInEasing)
             )
         ) { page ->
-
             val pageOffset by remember {
                 derivedStateOf {
                     ((state.currentPage - page) + state.currentPageOffsetFraction).absoluteValue
@@ -166,33 +189,32 @@ private fun VerticalPicker(
             val distance = abs(state.currentPage - page)
             val fontSize = (baseStyle.fontSize.value - distance).coerceAtLeast(14f).sp
 
-
             Box(
                 modifier = Modifier
-                    .fillMaxHeight(),
+                    .fillMaxHeight()
+                    .wrapContentWidth(),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
                     text = options.getOrNull(page) ?: "",
                     style = baseStyle.copy(fontSize = fontSize),
                     modifier = Modifier.fillMaxWidth(),
-                    textAlign =  TextAlign.Start,
+                    textAlign = TextAlign.Start,
                     color = Theme.colorScheme.shadePrimary.copy(alpha = alpha)
                 )
             }
         }
+
         Box(
             modifier = Modifier
-                .fillMaxSize()
+                .matchParentSize()
                 .background(
                     brush = Brush.verticalGradient(
                         colors = listOf(
                             Color(0x80F2F4F7),
                             Color.Transparent,
                             Color(0xE5F2F4F7)
-                        ),
-                        startY = 0f,
-                        endY = Float.POSITIVE_INFINITY
+                        )
                     )
                 )
         )
