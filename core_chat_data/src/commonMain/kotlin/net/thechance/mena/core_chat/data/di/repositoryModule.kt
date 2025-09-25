fab3ec6e4c8d80e73acf5a7b3ee815119e35f199
bilalazzam <blalazam065@gmail.com>
2025-09-25 09:26:45 +0300

    provide ChatRepository through di

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/repositoryModule.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/repositoryModule.kt
@@ -1,6 +1,8 @@
 package net.thechance.mena.core_chat.data.di
 
+import net.thechance.mena.core_chat.data.chat.ChatRepositoryImpl
 import net.thechance.mena.core_chat.data.contacts.ContactsRepositoryImpl
+import net.thechance.mena.core_chat.domain.repository.ChatRepository
 import net.thechance.mena.core_chat.domain.repository.ContactsRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -8,4 +10,5 @@ import org.koin.dsl.module
 
 internal val repositoryModule = module {
     singleOf(::ContactsRepositoryImpl) bind ContactsRepository::class
+    singleOf(::ChatRepositoryImpl) bind ChatRepository::class
 }
