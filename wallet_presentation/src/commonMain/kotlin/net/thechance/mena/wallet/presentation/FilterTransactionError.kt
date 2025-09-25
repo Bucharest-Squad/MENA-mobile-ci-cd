35d33682a6efbe28f1379021a5de0b27fa041180
Gitex <gehadepada@gmail.com>
2025-09-25 09:56:44 +0300

    feat:upload filter error image by different sizes

---
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/FilterTransactionError.kt
@@ -0,0 +1,24 @@
+package net.thechance.mena.wallet.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import mena.wallet_presentation.generated.resources.Res
+import mena.wallet_presentation.generated.resources.img_filter_error
+import net.thechance.mena.designsystem.presentation.component.image.Image
+import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+
+@Composable
+fun FilterTransactionError {
+
+    val Icon = Res.drawable.img_filter_error
+
+
+
+}
