2e64e4c1cc83d586ef85c899f3cb0ce0121fe2e0
Malak Raef <malak.raef20@gmail.com>
2025-09-25 03:23:57 +0300

    feat: create WheelDatePicker.kt component

---
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/filter/datePicker/WheelDatePicker.kt
@@ -0,0 +1,200 @@
+package net.thechance.mena.wallet.presentation.component.filter.datePicker
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import net.thechance.mena.wallet.presentation.component.filter.datePicker.DateConstants.MAX_YEAR
+import net.thechance.mena.wallet.presentation.component.filter.datePicker.DateConstants.MIN_YEAR
+import net.thechance.mena.wallet.presentation.utils.getMonthName
+import net.thechance.mena.wallet.presentation.utils.getNumberOfDaysInMonth
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+
+@Composable
+fun WheelDatePicker(
+    dayPagerState: PagerState,
+    monthPagerState: PagerState,
+    yearPagerState: PagerState,
+    modifier: Modifier = Modifier
+) {
+    val days = remember(monthPagerState.currentPage, yearPagerState.currentPage) {
+        val month = monthPagerState.currentPage + 1
+        val year = yearPagerState.currentPage + MIN_YEAR
+        (1..getNumberOfDaysInMonth(year, month))
+            .map { number -> if (number < 10) "0$number" else number.toString() }
+    }
+
+    LaunchedEffect(days.size) {
+        if (dayPagerState.currentPage >= days.size) {
+            dayPagerState.scrollToPage(days.lastIndex)
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    ) {
+
+        ChoiceIndicator(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 60.dp)
+                .align(Alignment.Center)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.weight(15f))
+
+            VerticalPicker(
+                state = dayPagerState,
+                options = days,
+                modifier = Modifier.weight(11f).fillMaxHeight()
+            )
+
+            VerticalPicker(
+                state = monthPagerState,
+                options = (1..12).map { getMonthName(it) },
+                modifier = Modifier.weight(29f).fillMaxHeight()
+            )
+
+            VerticalPicker(
+                state = yearPagerState,
+                options = (MIN_YEAR..MAX_YEAR).map { it.toString() },
+                modifier = Modifier.weight(11f).fillMaxHeight(),
+            )
+
+            Spacer(modifier = Modifier.weight(15f))
+        }
+    }
+}
+
+@Composable
+private fun ChoiceIndicator(modifier: Modifier = Modifier) {
+    val borderColor = Theme.colorScheme.stroke
+    Canvas(
+        modifier = modifier
+    ) {
+        val centerY = size.height / 2f
+        val itemHeight = 40.dp.toPx()
+        val topBorderY = centerY - (itemHeight / 2f)
+        val bottomBorderY = centerY + (itemHeight / 2f)
+
+        val strokeWidth = 1.dp.toPx()
+
+        drawLine(
+            color = borderColor,
+            start = Offset(0f, topBorderY),
+            end = Offset(size.width, topBorderY),
+            strokeWidth = strokeWidth
+        )
+
+        drawLine(
+            color = borderColor,
+            start = Offset(0f, bottomBorderY),
+            end = Offset(size.width, bottomBorderY),
+            strokeWidth = strokeWidth
+        )
+    }
+}
+
+@Composable
+private fun VerticalPicker(
+    state: PagerState,
+    options: List<String>,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+
+        VerticalPager(
+            state = state,
+            modifier = Modifier.fillMaxSize(),
+            beyondViewportPageCount = 5,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            pageSpacing = 4.dp,
+            contentPadding = PaddingValues(vertical = 80.dp),
+            pageSize = PageSize.Fixed(40.dp),
+            flingBehavior = PagerDefaults.flingBehavior(
+                state = state,
+                pagerSnapDistance = PagerSnapDistance.atMost(1),
+                snapAnimationSpec = tween(durationMillis = 200, easing = LinearOutSlowInEasing)
+            )
+        ) { page ->
+
+            val pageOffset by remember {
+                derivedStateOf {
+                    ((state.currentPage - page) + state.currentPageOffsetFraction).absoluteValue
+                }
+            }
+            val alpha = (1f - (pageOffset * 0.3f)).coerceAtLeast(0.3f)
+
+            val baseStyle = Theme.typography.body.large
+            val distance = abs(state.currentPage - page)
+            val fontSize = (baseStyle.fontSize.value - distance).coerceAtLeast(14f).sp
+
+
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = options.getOrNull(page) ?: "",
+                    style = baseStyle.copy(fontSize = fontSize),
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign =  TextAlign.Start,
+                    color = Theme.colorScheme.shadePrimary.copy(alpha = alpha)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0x80F2F4F7),
+                            Color.Transparent,
+                            Color(0xE5F2F4F7)
+                        ),
+                        startY = 0f,
+                        endY = Float.POSITIVE_INFINITY
+                    )
+                )
+        )
+    }
+}
