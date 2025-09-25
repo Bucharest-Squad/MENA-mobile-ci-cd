1c57b965f225c4e7f8c72a87b3bd95ba314e4e21
bilalazzam <blalazam065@gmail.com>
2025-09-25 16:32:23 +0300

    clean chat repository

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/repositoryModule.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/repositoryModule.kt
@@ -1,8 +1,6 @@
 package net.thechance.mena.core_chat.data.di
 
-import net.thechance.mena.core_chat.data.chat.ChatRepositoryImpl
 import net.thechance.mena.core_chat.data.contacts.ContactsRepositoryImpl
-import net.thechance.mena.core_chat.domain.repository.ChatRepository
 import net.thechance.mena.core_chat.domain.repository.ContactsRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -10,5 +8,4 @@ import org.koin.dsl.module
 
 internal val repositoryModule = module {
     singleOf(::ContactsRepositoryImpl) bind ContactsRepository::class
-    singleOf(::ChatRepositoryImpl) bind ChatRepository::class
 }
