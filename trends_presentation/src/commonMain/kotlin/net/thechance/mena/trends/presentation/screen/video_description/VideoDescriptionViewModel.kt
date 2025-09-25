828416d07d3b6e9ee9c6536374c8f49aff7525f3
Hend Sayed <hendsayed853@gmail.com>
2025-09-25 20:28:44 +0300

    use VideoDescriptionArgs in VideoDescriptionViewModel instead of custom save Sate handle

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/video_description/VideoDescriptionViewModel.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/video_description/VideoDescriptionViewModel.kt
@@ -1,25 +1,28 @@
 package net.thechance.mena.trends.presentation.screen.video_description
 
+import net.thechance.mena.trends.presentation.screen.video_description.args.VideoDescriptionArgs
 import net.thechance.mena.trends.presentation.shared.base.BaseViewModel
-import net.thechance.mena.trends.presentation.shared.util.ArgsConstant.TREND_ID
-import net.thechance.mena.trends.presentation.shared.util.save_state.CustomSaveState
 import org.koin.android.annotation.KoinViewModel
 import org.koin.core.annotation.Provided
 
 @KoinViewModel
 internal class VideoDescriptionViewModel(
-    @Provided customSaveStateHandleHandle: CustomSaveState,
+    @Provided private val videoDescriptionArgs: VideoDescriptionArgs
 ) : BaseViewModel<VideoDescriptionScreenState, VideoDescriptionEffect>(initialState = VideoDescriptionScreenState()),
     VideoDescriptionInteractionListener {
 
-    val trendId = customSaveStateHandleHandle.get<String>(TREND_ID).toString()
 
     override fun onBackClick() {
         sendEffect(VideoDescriptionEffect.NavigateBack)
     }
 
     override fun onNextClick() {
-        sendEffect(VideoDescriptionEffect.NavigateToSelectCategories(state.value.description, trendId))
+        sendEffect(
+            VideoDescriptionEffect.NavigateToSelectCategories(
+                state.value.description,
+                videoDescriptionArgs.trendId
+            )
+        )
     }
 
     override fun onDescriptionChanged(newValue: String) {
