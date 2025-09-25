b5c759d323296b49a5a812b5300719469128f917
Hend Sayed <hendsayed853@gmail.com>
2025-09-25 20:37:03 +0300

    remove unused files from trends_presentation module

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/util/ArgsConstant.kt
+++
@@ -1,5 +0,0 @@
-package net.thechance.mena.trends.presentation.shared.util
-
-object ArgsConstant {
-    const val TREND_ID = "trendId"
-}
--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/util/save_state/CustomSaveStateHandle.kt
+++
@@ -1,6 +0,0 @@
-package net.thechance.mena.trends.presentation.shared.util.save_state
-
-interface CustomSaveStateHandle {
-    fun <T> get(key:String) : T?
-    fun <T> set(key:String, value:T)
-}
