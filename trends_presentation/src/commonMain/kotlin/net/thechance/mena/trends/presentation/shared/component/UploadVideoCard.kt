2cdb14a11fd70f227c694fbd71bed1798a9f1bce
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-09-25 23:28:41 +0300

    refactor: remove unused import

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/UploadVideoCard.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/UploadVideoCard.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.unit.dp
 import mena.trends_presentation.generated.resources.Res
 import mena.trends_presentation.generated.resources.available_video_format
 import mena.trends_presentation.generated.resources.ic_trend_upload
-import mena.trends_presentation.generated.resources.test_pic
 import mena.trends_presentation.generated.resources.upload_your_video
 import net.thechance.mena.designsystem.presentation.component.icon.Icon
 import net.thechance.mena.designsystem.presentation.component.image.Image
