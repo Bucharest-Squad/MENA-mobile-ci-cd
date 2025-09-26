c1a94b61093dd713b79de4837133fdda6ac914b5
Noor Serry <nour.serry.94@gmail.com>
2025-09-26 11:37:46 +0300

    feature : create fake chat repository

--- core_chat_data/build.gradle.kts
+++ core_chat_data/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
 
             implementation(libs.bundles.ktor)
             implementation(libs.androidx.datastore.preferences)
+            implementation(libs.kotlinx.datetime)
 
         }
         commonTest.dependencies {
---
+++ core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/repository/ChatRepository.kt
@@ -0,0 +1,16 @@
+package net.thechance.mena.core_chat.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+import net.thechance.mena.core_chat.domain.entity.Chat
+import net.thechance.mena.core_chat.domain.entity.Message
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@ExperimentalUuidApi
+interface ChatRepository {
+    suspend fun sendMessage(message: Message)
+    suspend fun loadMessages(chatId: Uuid): List<Message>
+    suspend fun markMessagesAsRead(chatId: Uuid)
+    fun subscribeToMessages(chatId: Uuid): Flow<Message>
+    suspend fun getChatById(chatId: Uuid): Chat
+}
