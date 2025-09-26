7f80eae2d7bacc043d68518bdb15ab04695637de
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-09-26 23:41:34 +0300

    Refactor: Use Theme.typography.body.small

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/filter/FilterContent.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/filter/FilterContent.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -50,7 +49,7 @@ fun FilterContent(
 
         Text(
             text = stringResource(Res.string.type),
-            style = MaterialTheme.typography.bodySmall,
+            style = Theme.typography.body.small,
             color = Theme.colorScheme.shadePrimary
         )
 
@@ -61,7 +60,7 @@ fun FilterContent(
 
         Text(
             text = stringResource(Res.string.status),
-            style = MaterialTheme.typography.bodySmall,
+            style = Theme.typography.body.small,
             color = Theme.colorScheme.shadePrimary
         )
 
@@ -159,7 +158,7 @@ private fun DatePickerField(
     ) {
         Text(
             text = label,
-            style = MaterialTheme.typography.bodySmall,
+            style = Theme.typography.body.small,
             color = Theme.colorScheme.shadePrimary
         )
 
