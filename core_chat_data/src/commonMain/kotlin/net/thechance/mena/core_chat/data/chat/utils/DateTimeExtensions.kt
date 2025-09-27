95550c71d08a814b2f2ad798a42e13f93ae2b41e
bilalazzam <blalazam065@gmail.com>
2025-09-27 17:26:14 +0300

    navigate from contacts to chat

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/chat/utils/DateTimeExtensions.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/chat/utils/DateTimeExtensions.kt
@@ -1,8 +1,10 @@
 package net.thechance.mena.core_chat.data.chat.utils
+
 import kotlinx.datetime.DatePeriod
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Clock
