b59dfccbcc1105ef32a6d7b1b2d4b269c62c18d4
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-09-28 15:59:18 +0300

    feature: add test mapper.

---
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/contacts/TestMapper.kt
@@ -0,0 +1,16 @@
+package net.thechance.mena.core_chat.data.contacts
+
+class TestMapper {
+
+    fun test() {
+        println("Test")
+    }
+
+    fun test2() {
+        println("Test2")
+    }
+
+    fun test3() {
+        println("Test3")
+    }
+}
