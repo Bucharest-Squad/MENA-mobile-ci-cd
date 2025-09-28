3812afa99a7944872c30293dc033edf3274bf36d
Gitex <gehadepada@gmail.com>
2025-09-28 22:26:35 +0300

    feat:add TransactionRepositoryImpl implementation

---
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package net.thechance.mena.wallet.data.repository.balance
+
+import net.thechance.mena.wallet.data.dto.TransactionDto
+import net.thechance.mena.wallet.data.exceptions.safeApiCall
+import net.thechance.mena.wallet.data.network_client.NetworkClient
+import net.thechance.mena.wallet.domain.entity.Transaction
+import net.thechance.mena.wallet.domain.model.TransactionFilter
+import net.thechance.mena.wallet.domain.repository.TransactionRepository
+
+class TransactionRepositoryImpl(
+    private val networkClient: NetworkClient
+) : TransactionRepository {
+    override suspend fun getTransactionHistory(transactionFilter: TransactionFilter?):List<Transaction>?{
+        return safeApiCall<List<Transaction>?> {
+              networkClient.get(Transaction_PATH)
+        }
+    }
+    private companion object {
+        const val Transaction_PATH = "wallet/transaction?"
+    }
+}
