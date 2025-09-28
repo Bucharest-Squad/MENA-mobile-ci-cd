bcc578babe1e695b06087aed4282c1c6b00fdce0
Hager Abobakr <hagarabobakr1@gmail.com>
2025-09-28 12:31:24 +0300

    refactor: Move mappers to a separate file

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/export/ExportTransactionsState.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/export/ExportTransactionsState.kt
@@ -1,6 +1,5 @@
 package net.thechance.mena.wallet.presentation.screen.export
 
-import net.thechance.mena.wallet.domain.entity.Transaction
 import net.thechance.mena.wallet.presentation.base.SnackBarState
 import net.thechance.mena.wallet.presentation.model.FilterStatus
 import net.thechance.mena.wallet.presentation.model.FilterType
@@ -23,18 +22,3 @@ data class ExportTransactionsState(
     val endDate: String? = "",
 )
 
-fun FilterType.toDomain(): Transaction.Type {
-    return when (this) {
-        FilterType.SENT -> Transaction.Type.SENT
-        FilterType.RECEIVED -> Transaction.Type.RECEIVED
-        FilterType.ONLINE_PURCHASE -> Transaction.Type.ONLINE_PURCHASE
-    }
-}
-
-fun FilterStatus.toDomain(): Transaction.Status? {
-    return when (this) {
-        FilterStatus.ALL -> null
-        FilterStatus.FAILED -> Transaction.Status.FAIL
-        FilterStatus.SUCCESS -> Transaction.Status.SUCCESS
-    }
-}
