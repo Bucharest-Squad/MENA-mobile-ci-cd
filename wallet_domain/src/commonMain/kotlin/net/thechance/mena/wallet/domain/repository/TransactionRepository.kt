52111eab268e5dcc8227b9b0f8d4ebc06dd301eb
Gitex <gehadepada@gmail.com>
2025-09-28 21:05:29 +0300

    feat : add TransactionRepository interface

--- wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/repository/TransactionRepository.kt
+++ wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/repository/TransactionRepository.kt
@@ -1,4 +1,9 @@
 package net.thechance.mena.wallet.domain.repository
 
+import net.thechance.mena.wallet.domain.entity.Transaction
+import net.thechance.mena.wallet.domain.model.TransactionFilter
+
+
 interface TransactionRepository {
+    suspend fun getTransactionHistory(transactionFilter:TransactionFilter?):List<Transaction>?
 }
