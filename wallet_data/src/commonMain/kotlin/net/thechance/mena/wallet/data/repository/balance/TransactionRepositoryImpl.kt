3746988d224288814e64be82c5ea6074322cde4c
Gitex <gehadepada@gmail.com>
2025-09-28 23:51:43 +0300

    refactor:map response to entity

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package net.thechance.mena.wallet.data.repository.balance
 
+import net.thechance.mena.wallet.data.dto.TransactionDto
 import net.thechance.mena.wallet.data.exceptions.safeApiCall
+import net.thechance.mena.wallet.data.mapper.toEntity
 import net.thechance.mena.wallet.data.network_client.NetworkClient
 import net.thechance.mena.wallet.domain.entity.Transaction
 import net.thechance.mena.wallet.domain.model.TransactionFilterParams
@@ -9,29 +11,30 @@ import net.thechance.mena.wallet.domain.repository.TransactionRepository
 class TransactionRepositoryImpl(
     private val networkClient: NetworkClient
 ) : TransactionRepository {
-    override suspend fun getTransactionHistory(transactionFilterParams: TransactionFilterParams?):List<Transaction>?{
-        return safeApiCall<List<Transaction>?> {
-              networkClient.get("$TRANSACTION_PATH?${transactionFilterParams?.toQueryParams()}")
+    override suspend fun getTransactionHistory(transactionFilterParams: TransactionFilterParams?): List<Transaction>? {
+        return safeApiCall<List<TransactionDto>?> {
+            networkClient.get("$TRANSACTION_PATH?${transactionFilterParams?.toQueryParams()}")
+        }?.map { it.toEntity() }
+    }
 
-        }
-    }
     private companion object {
-        const val TRANSACTION_PATH= "wallet/transactions"
+        const val TRANSACTION_PATH = "wallet/transactions"
     }
-    private fun TransactionFilterParams.toQueryParams():String{
+
+    private fun TransactionFilterParams.toQueryParams(): String {
         val params = mutableListOf<String>()
-        type?.let{
+        type?.let {
             params.add("type=$it")
         }
-        status?.let{
+        status?.let {
             params.add("status=$it")
         }
-        startDate?.let{
+        startDate?.let {
             params.add("startDate=$it")
         }
-        endDate?.let{
+        endDate?.let {
             params.add("endDate=$it")
         }
-        return params.joinToString ("&" )
+        return params.joinToString("&")
     }
 }
