a387ee9e8868a45da8faad056e4c2da994a1b358
Gitex <gehadepada@gmail.com>
2025-09-28 21:58:57 +0300

    refactor:delete custom formate from transaction mapper

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/mapper/TransactionMapper.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/mapper/TransactionMapper.kt
@@ -1,8 +1,6 @@
 package net.thechance.mena.wallet.data.mapper
 
-import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.format.char
 import net.thechance.mena.wallet.data.dto.TransactionDto
 import net.thechance.mena.wallet.domain.entity.Transaction
 import net.thechance.mena.wallet.domain.model.TransactionStatus
@@ -22,10 +20,4 @@ fun TransactionDto.toEntity(): Transaction {
         type = TransactionType.valueOf(type),
     )
 
-}
-val customFormat = LocalDateTime.Format {
-    date(LocalDate.Formats.ISO)
-    char(' ')
-    hour(); char(':'); minute(); char(':'); second()
-    char(','); secondFraction(fixedLength = 3)
 }
