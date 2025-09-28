9930f5c7ab9df17508ad899637876699e5eda92c
ThoriaHamdy99 <thoriahamdy99@gmail.com>
2025-09-28 16:08:48 +0300

    feature: add test for test mapper.

---
+++ core_chat_data/src/commonTest/kotlin/net/thechance/mena/core_chat/data/contacts/TestMapperTest.kt
@@ -0,0 +1,12 @@
+package net.thechance.mena.core_chat.data.contacts
+
+import kotlin.test.Test
+
+class TestMapperTest {
+    private val testMapper = TestMapper()
+
+    @Test
+    fun `test should print Test`() {
+        testMapper.test()
+    }
+}
