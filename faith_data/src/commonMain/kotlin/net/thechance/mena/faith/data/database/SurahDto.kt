e418beeca96b1c9c47b0150d512ceec660531bf5
Marwan Qashwa <marawankashwa24@gmail.com>
2025-09-29 02:04:54 +0300

    fix: Update ColumnInfo annotation for ayahCount

--- faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/database/SurahDto.kt
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/database/SurahDto.kt
@@ -5,5 +5,5 @@ import androidx.room.ColumnInfo
 data class SurahDto(
     @ColumnInfo(name = "sura_no") val order: Int,
     @ColumnInfo(name = "sura_name_en") val name: String,
-    @ColumnInfo(name = "ayahCount") val ayahCount: Int
+    val ayahCount: Int
 )
