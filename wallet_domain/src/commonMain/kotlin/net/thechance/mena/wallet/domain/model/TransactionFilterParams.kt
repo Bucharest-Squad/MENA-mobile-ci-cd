01950fa734b5d55770392cd778ee19808be0f78d
Gitex <gehadepada@gmail.com>
2025-09-29 00:08:16 +0300

    fix:fix TransactionFilterParams to take many types

--- wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/model/TransactionFilterParams.kt
+++ wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/model/TransactionFilterParams.kt
@@ -3,7 +3,7 @@ package net.thechance.mena.wallet.domain.model
 import kotlinx.datetime.LocalDate
 
 data class TransactionFilterParams(
-    val type: TransactionType?,
+    val types: List<TransactionType>?,
     val status: TransactionStatus?,
     val startDate: LocalDate?,
     val endDate :LocalDate?,
