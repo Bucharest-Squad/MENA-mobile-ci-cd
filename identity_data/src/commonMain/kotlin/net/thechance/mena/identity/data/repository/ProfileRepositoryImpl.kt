2183d96a3aaae2d9dfba9bd5834b74d81ffe1b5f
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-09-28 08:19:43 +0300

    refactor: move user data source logic into ProfileRepository

--- identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/repository/ProfileRepositoryImpl.kt
+++ identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/repository/ProfileRepositoryImpl.kt
@@ -1,33 +1,49 @@
 package net.thechance.mena.identity.data.repository
 
-import net.thechance.mena.identity.data.datasource.localDataSource.UserLocalDataSource
-import net.thechance.mena.identity.data.datasource.remoteDataSource.UserRemoteDataSource
+import com.russhwolf.settings.Settings
+import io.ktor.client.HttpClient
+import kotlinx.serialization.json.Json
+import net.thechance.mena.identity.data.dto.profile.ProfileResponseDto
+
 import net.thechance.mena.identity.data.mapper.toDomain
+import net.thechance.mena.identity.data.mapper.toDto
+import net.thechance.mena.identity.data.utils.getJson
 import net.thechance.mena.identity.data.utils.safeWrapper
+import net.thechance.mena.identity.data.utils.user
 import net.thechance.mena.identity.domain.model.UserInfo
 import net.thechance.mena.identity.domain.repository.ProfileRepository
 
 
 class ProfileRepositoryImpl(
-    private val remoteDataSource: UserRemoteDataSource,
-    private val localDataSource: UserLocalDataSource
+    private val client: HttpClient,
+    private val settings: Settings,
+    private val json: Json = Json { encodeDefaults = true; ignoreUnknownKeys = true }
 ): ProfileRepository {
 
 
     override suspend fun fetchUserInfo(): UserInfo {
-        val userInfo = safeWrapper {
-            remoteDataSource.getUserInfo().toDomain()
+        safeWrapper {
+            val userInfo: ProfileResponseDto = client.getJson(path =PROFILE)
+            saveUserInfo(userInfo.toDomain())
         }
-        saveUserInfo(userInfo)
-        return userInfo
+        return getUserInfoFromLocal()!!
     }
 
     override suspend fun getUserInfoFromLocal(): UserInfo?{
-        return localDataSource.getUserInfo()
+        val userJson = settings.user
+        return userJson.let{ runCatching { json.decodeFromString<ProfileResponseDto>(it).toDomain()}.getOrNull() }
     }
 
     private fun saveUserInfo(userInfo: UserInfo){
-        localDataSource.saveUserInfo(userInfo)
+        val userJson = json.encodeToString(userInfo.toDto())
+        settings.user = userJson
+
     }
+
+
+    companion object{
+        const val PROFILE = "profile/me"
+    }
+
 
 }
--- identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/utils/settings.kt
+++ identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/utils/settings.kt
@@ -2,6 +2,7 @@ package net.thechance.mena.identity.data.utils
 
 import com.russhwolf.settings.Settings
 
+
 internal var Settings.accessToken: String
     get() = getString(ACCESS_TOKEN, "")
     set(value) = putString(ACCESS_TOKEN, value)
@@ -10,10 +11,15 @@ internal var Settings.refreshToken: String
     get() = getString(REFRESH_TOKEN, "")
     set(value) = putString(REFRESH_TOKEN, value)
 
+internal var Settings.user:String
+    get() = getString(USER_KEY, "")
+    set(value) = putString(USER_KEY,value)
+
 
 //region keys
 
 const val ACCESS_TOKEN = "access_token"
 const val REFRESH_TOKEN = "refresh_token"
+const val USER_KEY = "user"
 
 //endregion
