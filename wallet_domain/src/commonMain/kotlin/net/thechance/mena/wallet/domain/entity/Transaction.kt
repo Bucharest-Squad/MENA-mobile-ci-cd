3549ad6ddc3dd6f361d0bea9f89754f9ec60d009
Gitex <gehadepada@gmail.com>
2025-09-28 20:56:49 +0300

    refactor:refactor Transaction entity to use new enums

--- wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/entity/Transaction.kt
+++ wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/entity/Transaction.kt
@@ -1,6 +1,8 @@
 package net.thechance.mena.wallet.domain.entity
 
 import kotlinx.datetime.LocalDateTime
+import net.thechance.mena.wallet.domain.model.TransactionStatus
+import net.thechance.mena.wallet.domain.model.TransactionType
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -9,21 +11,10 @@ data class Transaction (
     val id: Uuid,
     val createdAt: LocalDateTime,
     val amount: Double,
-    val status: Status,
+    val status: TransactionStatus,
     val senderId: Uuid,
     val senderName: String,
     val receiverId: Uuid,
     val receiverName: String,
-    val type: Type
-) {
-    enum class Type {
-        SENT,
-        RECEIVED,
-        ONLINE_PURCHASE
-    }
-
-    enum class Status {
-        SUCCESS,
-        FAIL
-    }
-}
+    val type: TransactionType
+)
