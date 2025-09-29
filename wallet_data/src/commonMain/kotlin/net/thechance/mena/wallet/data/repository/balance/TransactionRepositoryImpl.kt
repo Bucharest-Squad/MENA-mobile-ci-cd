71a1a761b665f2e0032df261ccb2b3ce3066cd12
Gitex <gehadepada@gmail.com>
2025-09-29 07:17:03 +0300

    refactor:move filter params to separated file

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
@@ -3,11 +3,11 @@ package net.thechance.mena.wallet.data.repository.balance
 import net.thechance.mena.wallet.data.dto.PagedTransactionResponseDto
 import net.thechance.mena.wallet.data.exceptions.safeApiCall
 import net.thechance.mena.wallet.data.mapper.toEntity
+import net.thechance.mena.wallet.data.mapper.toParameters
 import net.thechance.mena.wallet.data.network_client.NetworkClient
 import net.thechance.mena.wallet.domain.entity.Transaction
 import net.thechance.mena.wallet.domain.model.TransactionFilterParams
 import net.thechance.mena.wallet.domain.repository.TransactionRepository
-import io.ktor.http.Parameters
 
 class TransactionRepositoryImpl(
     private val networkClient: NetworkClient
@@ -23,13 +23,4 @@ class TransactionRepositoryImpl(
         const val TRANSACTION_PATH = "wallet/transactions"
     }
 
-    private fun TransactionFilterParams.toParameters(): Parameters =
-        Parameters.build {
-            types?.forEach {
-                append("type", it.name)
-            }
-            status?.let { append("status", it.name) }
-            startDate?.let { append("startDate", it.toString()) }
-            endDate?.let { append("endDate", it.toString()) }
-        }
 }
