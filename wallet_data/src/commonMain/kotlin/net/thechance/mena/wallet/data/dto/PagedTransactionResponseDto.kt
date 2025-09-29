b1735b05156909eada0a57a0c71c91cc44bee79e
Gitex <gehadepada@gmail.com>
2025-09-29 05:46:45 +0300

    refactor:rename PagedTransactionResponseDto.kt

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/dto/PagedTransactionResponseDto.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/dto/PagedTransactionResponseDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PagedTransactionResponse (
+data class PagedTransactionResponseDto (
     @SerialName("totalElements")
     val totalElements : Long ,
     @SerialName("page")
