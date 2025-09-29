0ecd06630d75b0440481212042cb089282607b1b
Gitex <gehadepada@gmail.com>
2025-09-29 05:55:47 +0300

    refactor:update transaction repo interface to return PagedTransactionResponse

--- wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/repository/TransactionRepository.kt
+++ wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/repository/TransactionRepository.kt
@@ -1,9 +1,10 @@
 package net.thechance.mena.wallet.domain.repository
 
+import net.thechance.mena.wallet.domain.entity.PagedTransactionResponse
 import net.thechance.mena.wallet.domain.entity.Transaction
 import net.thechance.mena.wallet.domain.model.TransactionFilterParams
 
 
 interface TransactionRepository {
-    suspend fun getTransactionHistory(transactionFilterParams:TransactionFilterParams?):List<Transaction>
+    suspend fun getTransactionHistory(transactionFilterParams:TransactionFilterParams?):List<PagedTransactionResponse>
 }
