7c965c0361c65fa0758330cc5505da6df835aba3
Hager Abobakr <hagarabobakr1@gmail.com>
2025-09-29 10:58:12 +0300

    test: Update tests in ExportTransactionsViewModelTest

--- wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/export/ExportTransactionsViewModelTest.kt
+++ wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/export/ExportTransactionsViewModelTest.kt
@@ -3,7 +3,6 @@ package net.thechance.mena.wallet.presentation.screen.export
 import app.cash.turbine.test
 import dev.mokkery.MockMode
 import dev.mokkery.answering.returns
-import dev.mokkery.answering.throws
 import dev.mokkery.everySuspend
 import dev.mokkery.matcher.any
 import dev.mokkery.mock
@@ -14,10 +13,9 @@ import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import mena.wallet_presentation.generated.resources.Res
-import mena.wallet_presentation.generated.resources.error_no_transactions
-import net.thechance.mena.wallet.domain.exceptions.NoInternetException
 import net.thechance.mena.wallet.domain.repository.ExportTransactionsRepository
+import net.thechance.mena.wallet.presentation.base.CustomToastState
+import net.thechance.mena.wallet.presentation.base.SnackBarState
 import net.thechance.mena.wallet.presentation.model.FilterStatus
 import net.thechance.mena.wallet.presentation.model.FilterType
 import kotlin.test.AfterTest
@@ -128,18 +126,63 @@ class ExportTransactionsViewModelTest {
         }
     }
 
-//    @Test
-//    fun `onViewAndShareClicked with non-empty pdf should navigate`() = runTest {
-//        everySuspend  { repository.getFilteredTransactionsFile(any()) } returns byteArrayOf(1, 2, 3)
-//
-//        val viewModel = ExportTransactionsViewModel(repository, testDispatcher)
-//        viewModel.onViewAndShareClicked()
-//        advanceUntilIdle()
-//
-//        viewModel.uiEffect.test {
-//            val effect = awaitItem()
-//            assertTrue(effect is ExportTransactionsEffect.NavigateToViewFileScreen)
-//        }
-//    }
+    @Test
+    fun `should update startDate when onFromDateClicked is called`() = runTest {
+        val viewModel = ExportTransactionsViewModel(repository, testDispatcher)
+
+        viewModel.state.test {
+            skipItems(1)
+            viewModel.onFromDateClicked()
+            val state = awaitItem()
+            assertEquals("2025/09/01", state.startDate)
+        }
+    }
+
+    @Test
+    fun `should update endDate when onToDateClicked is called`() = runTest {
+        val viewModel = ExportTransactionsViewModel(repository, testDispatcher)
+
+        viewModel.state.test {
+            skipItems(1)
+            viewModel.onToDateClicked()
+            val state = awaitItem()
+            assertEquals("2025/09/27", state.endDate)
+        }
+    }
+
+
+    @Test
+    fun `onDownloadClicked with empty pdf should show toast`() = runTest {
+        everySuspend { repository.getFilteredTransactionsFile(any()) } returns byteArrayOf()
+
+        val viewModel = ExportTransactionsViewModel(repository, testDispatcher)
+
+        viewModel.state.test {
+            viewModel.onDownloadClicked()
+            skipItems(2)
+
+            val toastState = awaitItem().toast
+            assertToastState(isVisible = true, toastState = toastState)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    private fun assertSnackBarState(
+        isVisible: Boolean,
+        isSuccess: Boolean,
+        snackBarState: SnackBarState
+    ) {
+        assertEquals(isVisible, snackBarState.isVisible)
+        assertEquals(isSuccess, snackBarState.isSuccess)
+    }
+
+    private fun assertToastState(
+        isVisible: Boolean,
+        toastState: CustomToastState
+    ) {
+        assertEquals(isVisible, toastState.isVisible)
+    }
+
 
 }
