4cb730275e85544df0ede6b32890ab79bdd0d826
Gitex <gehadepada@gmail.com>
2025-09-29 06:31:48 +0300

    refactor:update TransactionRepositoryImpl.kt

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
@@ -7,13 +7,14 @@ import net.thechance.mena.wallet.data.network_client.NetworkClient
 import net.thechance.mena.wallet.domain.entity.Transaction
 import net.thechance.mena.wallet.domain.model.TransactionFilterParams
 import net.thechance.mena.wallet.domain.repository.TransactionRepository
+import io.ktor.http.Parameters
 
 class TransactionRepositoryImpl(
     private val networkClient: NetworkClient
 ) : TransactionRepository {
     override suspend fun getTransactionHistory(transactionFilterParams: TransactionFilterParams?): List<Transaction> {
         return safeApiCall<PagedTransactionResponseDto> {
-            networkClient.get("$TRANSACTION_PATH?${transactionFilterParams?.toQueryParams()}")
+            networkClient.get("$TRANSACTION_PATH?${transactionFilterParams?.toParameters()}")
         }.transactions.map { it.toEntity() }
 
     }
@@ -22,20 +23,13 @@ class TransactionRepositoryImpl(
         const val TRANSACTION_PATH = "wallet/transactions"
     }
 
-    private fun TransactionFilterParams.toQueryParams(): String {
-        val params = mutableListOf<String>()
-        type?.let {
-            params.add("type=$it")
+    private fun TransactionFilterParams.toParameters(): Parameters =
+        Parameters.build {
+            types?.forEach {
+                append("type", it.name)
+            }
+            status?.let { append("status", it.name) }
+            startDate?.let { append("startDate", it.toString()) }
+            endDate?.let { append("endDate", it.toString()) }
         }
-        status?.let {
-            params.add("status=$it")
-        }
-        startDate?.let {
-            params.add("startDate=$it")
-        }
-        endDate?.let {
-            params.add("endDate=$it")
-        }
-        return params.joinToString("&")
-    }
 }
