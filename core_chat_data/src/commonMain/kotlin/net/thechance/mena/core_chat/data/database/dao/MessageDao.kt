5eb913f0f9c4cedf273a152f6785e7f9ca01b230
Radwa85 <radwa.mohamed5033@gmail.com>
2025-10-02 00:35:49 +0300

    Change getMessagesByChat to a suspend function

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/database/dao/MessageDao.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/database/dao/MessageDao.kt
@@ -13,7 +13,7 @@ interface MessageDao {
     suspend fun insertMessage(message: MessageEntity)
 
     @Query("SELECT * FROM MessageEntity WHERE chat_id = :chatId ORDER BY timestamp ASC")
-    fun getMessagesByChat(chatId: String): Flow<List<MessageEntity>>
+    suspend fun getMessagesByChat(chatId: String): List<MessageEntity>
 
     @Query("UPDATE MessageEntity SET message_status = :status WHERE id = :id")
     suspend fun updateMessageStatus(id: String, status: MessageStatus)
