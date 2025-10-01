d01098169ca5f38f598cd5df56ae978f88cbe7c7
a7med naser <ad0112727@gmail.com>
2025-10-02 01:38:56 +0300

    refactor: remove unused imports from UploadProductImage.kt

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/productImage/UploadProductImage.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/productImage/UploadProductImage.kt
@@ -11,13 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import io.github.vinceglb.filekit.PlatformFile
 import io.github.vinceglb.filekit.dialogs.FileKitType
