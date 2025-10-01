efedc412b2894ee9d144de65f169bdd1d6255a0c
Aboud-B <78660324+ABDULLAHHG@users.noreply.github.com>
2025-10-01 10:33:48 -0700

    Feature/idn 59 design and implement phone number and otp screen (#239)
    * feat: implement forget password screen UI and logic
    
    * feat: implement OTP request logic in forget password
    
    * feat: add reusable authentication components
    
    * feat: add invalid OTP exception handling
    
    * feat: add forget password OTP screen
    
    * refactor: rename screen models to view models
    
    * feat: add forget password repository
    
    * feat: add cursor with blinking animation to OtpInput.
    
    * feat: localize forget password and OTP screens
    
    * refactor: rename forget password and otp screen packages
    
    * feat: handle OTP expiration and too many requests
    
    * feat: update forgot password OTP flow
    
    * test: add unit tests for `ForgetPasswordRepository`
    
    * refactor: move ErrorSnackBar out of Scaffold
    
    * test: add unit tests for forget password and otp view models
    
    * feat: add left arrow icon
    
    * Update and fix identity presentation tests
    
    * test: Add tests for ForgetPasswordRepository requestOTP
    
    * refactor: Rename ForgetPassword to ResetPassword
    
    * test: Add test for resetPassword success case
    
    * refactor: Pass `callingCode` for reset password flow
    
    * refactor: Update Invalid OTP error message to incorrect OTP
    
    * refactor: Make dependencies in ForgetPasswordScreenViewModel private
    
    * Move forget password and OTP view model tests to jvmTest
    
    * remove unused imports
    
    * refactor: Rename OTP to Otp
    
    * refactor: remove phone number from VerifyOtpRequestDto
    
    * refactor: Rename interaction listener methods

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/di/identityScreensModule.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/di/identityScreensModule.kt
@@ -1,6 +1,7 @@
 package net.thechance.mena.identity.presentation.di
 
-import net.thechance.mena.identity.presentation.screen.forget_password.ForgetPasswordScreenModel
+import net.thechance.mena.identity.presentation.screen.forgetPassword.ForgetPasswordScreenViewModel
+import net.thechance.mena.identity.presentation.screen.forgetPasswordOtp.OtpScreenViewModel
 import net.thechance.mena.identity.presentation.screen.login.LoginScreenModel
 import net.thechance.mena.identity.presentation.screen.register.RegisterScreenModel
 import org.koin.core.module.dsl.factoryOf
@@ -9,5 +10,6 @@ import org.koin.dsl.module
 val identityScreensModule = module {
     factoryOf(::LoginScreenModel)
     factoryOf(::RegisterScreenModel)
-    factoryOf(::ForgetPasswordScreenModel)
+    factoryOf(::ForgetPasswordScreenViewModel)
+    factoryOf(::OtpScreenViewModel)
 }
