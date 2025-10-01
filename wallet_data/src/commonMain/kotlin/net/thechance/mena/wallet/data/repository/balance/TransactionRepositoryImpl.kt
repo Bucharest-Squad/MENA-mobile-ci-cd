40a62783a5974c669505d9654fe9d874a24e776b
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-01 22:43:08 +0300

    refactor: stop loading when sharing screen

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
@@ -1,10 +1,5 @@
 package net.thechance.mena.wallet.data.repository.balance
 
-import io.ktor.client.request.parameter
-import io.ktor.http.Parameters
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.LocalTime
 import net.thechance.mena.wallet.data.dto.PagedTransactionResponseDto
 import net.thechance.mena.wallet.data.dto.TransactionDto
 import net.thechance.mena.wallet.data.exceptions.safeApiCall
@@ -13,8 +8,6 @@ import net.thechance.mena.wallet.data.mapper.toParameters
 import net.thechance.mena.wallet.data.network_client.NetworkClient
 import net.thechance.mena.wallet.domain.entity.Transaction
 import net.thechance.mena.wallet.domain.model.TransactionFilterParams
-import net.thechance.mena.wallet.domain.model.TransactionStatus
-import net.thechance.mena.wallet.domain.model.TransactionType
 import net.thechance.mena.wallet.domain.repository.TransactionRepository
 import org.koin.core.annotation.Single
 import kotlin.uuid.ExperimentalUuidApi
@@ -33,35 +26,13 @@ class TransactionRepositoryImpl(
     }
 
     override suspend fun getAllTransaction(): List<Transaction> {
-        return listOf(
-            Transaction(
-                id = Uuid.parse(""),
-                createdAt = LocalDateTime(
-                    date = LocalDate(2025, 8, 20),
-                    time = LocalTime(12, 0)
-                ),
-                amount = 5000.0,
-                status = TransactionStatus.SUCCESS,
-                senderName = "Nour Elhoda",
-                receiverName = "Nour Elhoda",
-                type = TransactionType.RECEIVED
-            )
-        )
+        TODO("Not yet implemented")
     }
 
     override suspend fun getTransactionById(transactionId: Uuid): Transaction {
-        return Transaction(
-            id = Uuid.parse(""),
-            createdAt = LocalDateTime(
-                date = LocalDate(2025, 8, 20),
-                time = LocalTime(12, 0)
-            ),
-            amount = 5000.0,
-            status = TransactionStatus.SUCCESS,
-            senderName = "Nour Elhoda",
-            receiverName = "Nour Elhoda",
-            type = TransactionType.RECEIVED
-        )
+        return safeApiCall<TransactionDto> {
+            networkClient.get("$TRANSACTION_PATH/$transactionId")
+        }.toEntity()
     }
 
     private companion object {
