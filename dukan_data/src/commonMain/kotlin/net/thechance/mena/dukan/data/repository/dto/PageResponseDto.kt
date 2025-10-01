7d023a626f632dffa8378c9265db7b09bd31a473
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-10-01 13:30:22 +0300

    aadd serial name for dto's properties

--- dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/repository/dto/PageResponseDto.kt
+++ dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/repository/dto/PageResponseDto.kt
@@ -5,11 +5,24 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PageResponseDto<T>(
+    @SerialName("content")
     val content: List<T>,
+
+    @SerialName("number")
     val number: Int,
+
+    @SerialName("size")
     val size: Int,
+
+    @SerialName("totalPages")
     val totalPages: Int,
+
+    @SerialName("totalElements")
     val totalElements: Long,
+
+    @SerialName("first")
     val first: Boolean,
+
+    @SerialName("last")
     val last: Boolean
 )
