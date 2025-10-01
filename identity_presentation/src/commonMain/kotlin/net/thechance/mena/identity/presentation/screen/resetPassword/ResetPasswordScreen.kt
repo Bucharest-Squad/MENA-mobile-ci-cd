b8ca10a5b97d7dc7c99fc6671f2db6d3dcf71530
shahad-97 <shos8406@gmail.com>
2025-10-01 23:38:04 +0300

    use AuthAppBar and ErrorSnackBar in ResetPasswordScreen

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/resetPassword/ResetPasswordScreen.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/resetPassword/ResetPasswordScreen.kt
@@ -1,6 +1,5 @@
 package net.thechance.mena.identity.presentation.screen.resetPassword
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,38 +7,28 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import mena.identity_presentation.generated.resources.Res
 import mena.identity_presentation.generated.resources.confirm_password_label
-import mena.identity_presentation.generated.resources.ic_close_eye
-import mena.identity_presentation.generated.resources.ic_lock
-import mena.identity_presentation.generated.resources.ic_open_eye
-import mena.identity_presentation.generated.resources.login_background
 import mena.identity_presentation.generated.resources.new_password_title
 import mena.identity_presentation.generated.resources.reset
 import mena.identity_presentation.generated.resources.reset_password
 import mena.identity_presentation.generated.resources.reset_password_description
 import net.thechance.mena.designsystem.presentation.component.button.PrimaryButton
 import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
-import net.thechance.mena.designsystem.presentation.component.text.Text
-import net.thechance.mena.designsystem.presentation.component.textField.TextField
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.identity.presentation.base.BaseScreen
 import net.thechance.mena.identity.presentation.components.AuthAppBar
 import net.thechance.mena.identity.presentation.components.AuthScreenContainer
 import net.thechance.mena.identity.presentation.components.ErrorSnackBar
+import net.thechance.mena.identity.presentation.components.LabelInputPassword
 import net.thechance.mena.identity.presentation.components.PageDescription
 import net.thechance.mena.identity.presentation.screen.login.LoginScreen
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.core.parameter.parametersOf
 
@@ -50,7 +39,8 @@ data class ResetPasswordScreen(
     BaseScreen<ResetPasswordScreenViewModel,
             ResetPasswordScreenUIState,
             ResetPasswordScreenUIEffect,
-            ResetPasswordScreenInteractionListener>() {
+            ResetPasswordScreenInteractionListener>()
+    {
 
     @Composable
     override fun Content() {
@@ -75,70 +65,41 @@ data class ResetPasswordScreen(
                     .fillMaxSize()
                     .background(Theme.colorScheme.background.surface)
             ) {
-
-                Image(
-                    painter = painterResource(Res.drawable.login_background),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillWidth,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.BottomCenter)
-                )
-
-                AuthScreenContainer()
-                {
+                AuthScreenContainer() {
                     PageDescription(
                         title = stringResource(Res.string.new_password_title),
                         subtitle = stringResource(Res.string.reset_password_description),
                     )
 
-                    Text(
-                        text = stringResource(Res.string.new_password_title),
-                        style = Theme.typography.title.small,
-                        color = Theme.colorScheme.shadePrimary,
-                        modifier = Modifier.padding(bottom = Theme.spacing._4)
+                    LabelInputPassword(
+                        password = state.newPassword,
+                        isPasswordVisible = state.isNewPasswordVisible,
+                        onChangePassword = listener::onChangeNewPassword,
+                        onTogglePasswordVisibility = listener::onToggleNewPasswordVisibility,
+                        label = stringResource(Res.string.new_password_title),
+                        modifier = Modifier.padding(bottom = 16.dp)
                     )
 
-                    TextField(
-                        value = state.newPassword,
-                        onValueChanged = listener::onChangeNewPassword,
-                        hint = "",
-                        trailingIcon = painterResource(
-                            if (state.isNewPasswordVisible) Res.drawable.ic_open_eye
-                            else Res.drawable.ic_close_eye
-                        ),
-                        leadingIcon = painterResource(Res.drawable.ic_lock),
-                        showTrailingDivider = false,
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    LabelInputPassword(
+                        password = state.confirmPassword,
+                        isPasswordVisible = state.isConfirmPasswordVisible,
+                        onChangePassword = listener::onChangeConfirmPassword,
+                        onTogglePasswordVisibility = listener::onToggleConfirmPasswordVisibility,
+                        label = stringResource(Res.string.confirm_password_label),
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    PrimaryButton(
+                        text = stringResource(Res.string.reset),
+                        onClick = listener::onClickResetPassword,
+                        isEnabled = state.isResetEnabled,
+                        isLoading = state.isLoading,
+                        contentPadding = PaddingValues(vertical = Theme.spacing._12),
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(bottom = Theme.spacing._16),
-                        visualTransformation = if (state.isNewPasswordVisible) VisualTransformation.None
-                        else PasswordVisualTransformation(),
-                        onTrailingIconClick = listener::onToggleNewPasswordVisibility
-                    )
-
-                    Text(
-                        text = stringResource(Res.string.confirm_password_label),
-                        style = Theme.typography.title.small,
-                        color = Theme.colorScheme.shadePrimary,
-                        modifier = Modifier.padding(bottom = Theme.spacing._4)
-                    )
-
-                    TextField(
-                        value = state.confirmPassword,
-                        onValueChanged = listener::onChangeConfirmPassword,
-                        hint = "",
-                        trailingIcon = painterResource(
-                            if (state.isConfirmPasswordVisible) Res.drawable.ic_open_eye
-                            else Res.drawable.ic_close_eye
-                        ),
-                        leadingIcon = painterResource(Res.drawable.ic_lock),
-                        showTrailingDivider = false,
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                        modifier = Modifier.fillMaxWidth(),
-                        visualTransformation = if (state.isConfirmPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                        onTrailingIconClick = listener::onToggleConfirmPasswordVisibility
+                            .padding(bottom = Theme.spacing._24)
                     )
 
                     Spacer(modifier = Modifier.weight(1f))
@@ -155,11 +116,12 @@ data class ResetPasswordScreen(
                     )
                 }
             }
-            ErrorSnackBar(
-                errorMessage = state.errorMessage,
-                onDismiss = listener::onClearErrorMessage
-            )
         }
+        ErrorSnackBar(
+            errorMessage = state.errorMessage,
+            onDismiss = listener::onClearErrorMessage,
+            modifier = Modifier.statusBarsPadding()
+        )
     }
 
     override fun onEffect(
