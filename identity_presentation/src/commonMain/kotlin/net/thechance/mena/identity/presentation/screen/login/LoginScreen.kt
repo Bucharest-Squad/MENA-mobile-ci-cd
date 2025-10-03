4e6a001a83288e9cdcd2909a00d283b560464ee1
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-03 17:22:36 +0300

    refactor: resolve comments

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/login/LoginScreen.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/login/LoginScreen.kt
@@ -165,7 +165,7 @@ class LoginScreen(
         when (effect) {
             is LoginScreenUIEffect.NavigateToRegister -> navigator.push(RegisterScreen())
             LoginScreenUIEffect.NavigateToForgotPassword -> navigator.push(ForgetPasswordScreen())
-            LoginScreenUIEffect.NavigateToHome -> {}
+            LoginScreenUIEffect.NavigateToHome -> TODO()
         }
     }
 }
