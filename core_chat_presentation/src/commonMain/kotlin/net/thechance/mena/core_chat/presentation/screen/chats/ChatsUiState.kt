399d95f202d8a8174a7ddc3c3ddba7571d250a73
Mona <monaayman584@gmail.com>
2025-10-08 17:18:40 +0300

    feature: add attributes for ChatSMainScreen

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chats/ChatsUiState.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chats/ChatsUiState.kt
@@ -1,6 +1,15 @@
 package net.thechance.mena.core_chat.presentation.screen.chats
 
+import net.thechance.mena.core_chat.domain.entity.MessageStatus
+import net.thechance.mena.core_chat.presentation.screen.chat.TextMessageUiState
+
 data class ChatsUiState(
     val isLoading: Boolean = false,
-    val isSynced: Boolean = false
+    val isSynced: Boolean = false,
+    val name: String = "",
+    val lastMessage: String = "",
+    val isMenaUser: Boolean = false,
+    val messageStatus: MessageStatus = MessageStatus.SENT,
+    val balance: Double = 0.0,
+    val messageUiState: TextMessageUiState,
 )
