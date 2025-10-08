72e43313c582fe8e2c484a7ee5cc6f4f17aeff10
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-08 16:50:23 +0300

    refactor: use request body

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/payment/PaymentRepositoryImpl.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/payment/PaymentRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package net.thechance.mena.wallet.data.repository.payment
 
 import io.ktor.client.request.parameter
+import io.ktor.client.request.setBody
 import net.thechance.mena.wallet.data.dto.PaymentConfirmationDto
 import net.thechance.mena.wallet.data.exceptions.safeApiCall
 import net.thechance.mena.wallet.data.mapper.toEntity
 import net.thechance.mena.wallet.data.network_client.NetworkClient
 import net.thechance.mena.wallet.domain.model.PaymentConfirmation
+import net.thechance.mena.wallet.domain.model.TransactionStatus
 import net.thechance.mena.wallet.domain.repository.PaymentRepository
 import org.koin.core.annotation.Single
 
@@ -15,13 +17,26 @@ class PaymentRepositoryImpl(private val networkClient: NetworkClient): PaymentRe
         receiverId: String,
         amount: Double
     ): PaymentConfirmation {
+        val requestBody = PaymentConfirmationRequest(receiverId, amount)
         return safeApiCall<PaymentConfirmationDto> {
             networkClient.post(CONFIRM_PAYMENT_PATH) {
-                parameter(AMOUNT_PARAM, amount)
-                parameter(RECEIVER_ID_PARAM, receiverId)
+                setBody(requestBody)
             }
         }.toEntity()
+
+//        PaymentConfirmation(
+//            balance = 500045.0102,
+//            receiverName = "nour",
+//            receiverImg = "https://media.istockphoto.com/id/469738422/photo/large-boulders-on-lake-shore-at-sunset-minnesota-usa.jpg?s=612x612&w=0&k=20&c=4FzViDygZ8CgixTqt3VOudLJUP8uoSeh2UlD_qHYkAw=",
+//            status = TransactionStatus.SUCCESS
+//        )
+
     }
+
+    data class PaymentConfirmationRequest(
+        val receiverId: String,
+        val amount: Double
+    )
 
     private companion object {
         const val PAYMENT_PATH = "/wallet/payment"
