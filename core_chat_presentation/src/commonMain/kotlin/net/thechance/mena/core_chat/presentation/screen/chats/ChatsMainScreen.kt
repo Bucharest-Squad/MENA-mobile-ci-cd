9b39d708b56ce059ec9a6c0e46a0297fc7ff5a9e
Mona <monaayman584@gmail.com>
2025-10-09 11:07:55 +0300

    feature: update ui

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chats/ChatsMainScreen.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chats/ChatsMainScreen.kt
@@ -1,6 +1,7 @@
 package net.thechance.mena.core_chat.presentation.screen.chats
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,18 +11,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import mena.core_chat_presentation.generated.resources.Res
 import mena.core_chat_presentation.generated.resources.chats
-import mena.core_chat_presentation.generated.resources.ic_warning
+import mena.core_chat_presentation.generated.resources.ic_coin
+import mena.core_chat_presentation.generated.resources.ic_plus
 import mena.core_chat_presentation.generated.resources.mena
 import net.thechance.mena.core_chat.presentation.screen.chats.components.ChatCard
 import net.thechance.mena.designsystem.presentation.component.appBar.AppBar
+import net.thechance.mena.designsystem.presentation.component.button.FabButton
 import net.thechance.mena.designsystem.presentation.component.icon.Icon
 import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
@@ -29,14 +30,90 @@ import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalUuidApi::class)
 @Composable
 fun ChatsMainScreen(
-    chatsUiState: List<ChatsUiState> = emptyList(),
+    viewModel: ChatsViewModel = koinViewModel<ChatsViewModel>(),
     modifier: Modifier = Modifier
 ) {
+    val fakeData = listOf(
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "Sara Mahmoud",
+            imageUrl = "https://i.ibb.co/3y0TLrqV/f685ab4ed41c4c97bc4ffc6b3795175d4a29cd41.jpg",
+            lastMessage = "Hello Noha, nice to meet u!!",
+            time = "12:33 PM",
+            status = ChatsScreenState.ChatUiState.Status.UnRead(12),
+            isMine = false,
+        ),
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "Maryam Saleh",
+            imageUrl = "https://i.ibb.co/DjJLNHm/ef7bf477a8366d411f62a575dc169f0858ca1fec.jpg",
+            lastMessage = "You: Ok, thanks 😊",
+            time = "11:24 PM",
+            status = ChatsScreenState.ChatUiState.Status.Read,
+            isMine = true,
+        ),
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "Ayad Saddon",
+            imageUrl = null,
+            lastMessage = "Do you think going out in the rain right no...",
+            time = "Yesterday",
+            status = ChatsScreenState.ChatUiState.Status.UnRead(3),
+            isMine = false,
+        ),
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "Aseel Rahman",
+            imageUrl = "https://i.ibb.co/k2tY9Pcf/f538e7ed045b525721bd578ffa86d22fb58a9245.jpg",
+            lastMessage = "Shared 10 photos",
+            time = "17-03-2025",
+            status = ChatsScreenState.ChatUiState.Status.Received,
+            isMine = false,
+        ),
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "The princess 👑",
+            imageUrl = "https://i.ibb.co/XrjSgT95/6a9470e240d86a266e0d9fa666c6fce3e0659d2e.jpg",
+            lastMessage = "Shared a link",
+            time = "17-03-2025",
+            status = ChatsScreenState.ChatUiState.Status.Received,
+            isMine = false,
+        ),
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "Mansour Mogyad Ibrahee...",
+            imageUrl = "https://i.ibb.co/m5TCtW9w/afe181934c9bdfd994e1a0f1a9e8ecbb935908a9.jpg",
+            lastMessage = "You: Send a photo",
+            time = "17-03-2025",
+            status = ChatsScreenState.ChatUiState.Status.Sent,
+            isMine = true,
+        ),
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "Maha Fares",
+            imageUrl = "https://i.ibb.co/0RG3LYJB/10a780bcc5534f15472c4f67093c27d7ea30d861.jpg",
+            lastMessage = "Shared a photo",
+            time = "19-03-2025",
+            status = ChatsScreenState.ChatUiState.Status.Read,
+            isMine = false,
+        ),
+        ChatsScreenState.ChatUiState(
+            id = Uuid.random(),
+            name = "Aseel Rahman",
+            imageUrl = "https://i.ibb.co/tPYDxg0L/cedbd762f4ea4d209b0187b254769d0b89988d8e.jpg",
+            lastMessage = "★",
+            time = "17-03-2025",
+            status = ChatsScreenState.ChatUiState.Status.UnRead(12),
+            isMine = false,
+        )
+    )
     Box(
         modifier = modifier.fillMaxSize()
     ) {
@@ -47,20 +124,23 @@ fun ChatsMainScreen(
                 )
         ) {
             AppBar(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 18.dp),
                 title = stringResource(Res.string.mena),
                 trailingContent = {
                     Text(
-                        text = "134",
+                        text = "${viewModel.state.value.balance}",
                         color = Theme.colorScheme.shadeSecondary,
                         style = Theme.typography.label.small,
                         modifier = Modifier.padding(vertical = Theme.spacing._4),
                     )
                     Icon(
-                        painter = painterResource(Res.drawable.ic_warning),
+                        painter = painterResource(Res.drawable.ic_coin),
                         contentDescription = null,
+                        modifier = Modifier.size(24.dp).clickable {
+                            viewModel.onWalletClicked()
+                        }
                     )
                 }
-
             )
             Text(
                 text = stringResource(Res.string.chats),
@@ -68,7 +148,6 @@ fun ChatsMainScreen(
                 style = Theme.typography.title.small,
                 modifier = Modifier.fillMaxWidth().padding(horizontal = Theme.spacing._16)
             )
-
             LazyColumn(
                 modifier = Modifier.padding(horizontal = Theme.spacing._16)
                     .fillMaxWidth()
@@ -76,36 +155,19 @@ fun ChatsMainScreen(
                 contentPadding = PaddingValues(vertical = Theme.spacing._12),
                 verticalArrangement = Arrangement.spacedBy(Theme.spacing._16)
             ) {
-                items(chatsUiState) { chatUiState ->
+                items(fakeData.size) {
                     ChatCard(
-                        name = chatUiState.name,
-                        lastMessage = chatUiState.lastMessage,
-                        isMenaUser = chatUiState.isMenaUser,
-                        messageStatus = chatUiState.messageStatus,
-                        messageUiState = chatUiState.messageUiState,
-                        balance = chatUiState.balance,
-                        onClick = {},
-                        chatAvatarUrl = ""
+                        chats = fakeData[it],
                     )
                 }
             }
         }
-        Box(
-            modifier = Modifier.padding(Theme.spacing._16)
-                .size(56.dp)
-                .background(
-                    color = Theme.colorScheme.primary.primary,
-                    shape = RoundedCornerShape(Theme.radius.md)
-                )
-                .align(Alignment.BottomEnd),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_warning),
-                contentDescription = null,
-                tint = Theme.colorScheme.primary.onPrimary
-            )
-        }
+        FabButton(
+            painter = painterResource(Res.drawable.ic_plus),
+            onClick = viewModel::onNewChatClicked,
+            modifier = Modifier.align(Alignment.BottomEnd)
+                .padding(Theme.spacing._16)
+        )
     }
 }
 
--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chats/components/ChatCard.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chats/components/ChatCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,16 +20,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import kotlinx.datetime.LocalDateTime
 import mena.core_chat_presentation.generated.resources.Res
 import mena.core_chat_presentation.generated.resources.ic_message_read
 import mena.core_chat_presentation.generated.resources.ic_message_sent
 import mena.core_chat_presentation.generated.resources.ic_profile_placeholder
-import net.thechance.mena.core_chat.domain.entity.MessageStatus
-import net.thechance.mena.core_chat.presentation.screen.chat.MessageStatusUiState
-import net.thechance.mena.core_chat.presentation.screen.chat.TextMessageUiState
-import net.thechance.mena.core_chat.presentation.utils.formatAsTime
-import net.thechance.mena.core_chat.presentation.utils.now
+import net.thechance.mena.core_chat.presentation.screen.chats.ChatsScreenState
 import net.thechance.mena.designsystem.presentation.component.icon.Icon
 import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
@@ -36,86 +32,65 @@ import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Composable
 fun ChatCard(
-    name: String,
-    lastMessage: String,
-    isMenaUser: Boolean,
-    balance: Double,
-    messageStatus: MessageStatus,
-    messageUiState: TextMessageUiState,
-    onClick: () -> Unit,
-    chatAvatarUrl: String? = null,
+    chats: ChatsScreenState.ChatUiState,
+    modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .clickable { onClick() },
+            .clickable { },
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        if (
-            isMenaUser
-        ) {
-            Box(
-                modifier = Modifier.padding(end = Theme.spacing._8)
-                    .clip(CircleShape)
-                    .background(
-                        color = Theme.colorScheme.background.surfaceLow,
-                        shape = CircleShape
-                    )
-                    .size(48.dp)
-                    .padding(10.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                AsyncImage(
-                    modifier = Modifier.fillMaxSize(),
-                    model = chatAvatarUrl,
-                    placeholder = painterResource(Res.drawable.ic_profile_placeholder),
-                    error = painterResource(Res.drawable.ic_profile_placeholder),
-                    contentScale = ContentScale.Crop,
-                    contentDescription = "Contact photo",
+        Box(
+            modifier = Modifier.padding(end = Theme.spacing._8)
+                .clip(CircleShape)
+                .background(
+                    color = Theme.colorScheme.background.surfaceLow,
+                    shape = CircleShape
                 )
-
-            }
-        }
-
-        Column(
-            modifier = Modifier.padding(vertical = Theme.spacing._4).weight(1f).padding(end = Theme.spacing._4),
-            verticalArrangement = Arrangement.spacedBy(Theme.spacing._2)
+                .size(48.dp)
+                .padding(10.dp),
+            contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = name,
-                style = Theme.typography.label.medium,
-                color = Theme.colorScheme.shadePrimary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = lastMessage,
-                style = Theme.typography.label.small,
-                color = Theme.colorScheme.shadeTertiary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = chats.imageUrl,
+                placeholder = painterResource(Res.drawable.ic_profile_placeholder),
+                error = painterResource(Res.drawable.ic_profile_placeholder),
+                contentScale = ContentScale.Crop,
+                contentDescription = "Contact photo",
             )
         }
+        NameAndLastMessage(chats)
+        TimeAndStatus(chats)
+    }
+
+}
+
+    @Composable
+    private fun TimeAndStatus(chats: ChatsScreenState.ChatUiState) {
         Column(
             modifier = Modifier.padding(vertical = Theme.spacing._4),
             horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.spacedBy(Theme.spacing._2, Alignment.Top) ) {
+            verticalArrangement = Arrangement.spacedBy(Theme.spacing._2, Alignment.Top)
+        ) {
             Text(
-                text = messageUiState.sendTime.formatAsTime(),
+                text = chats.time,
                 style = Theme.typography.label.small,
                 color = Theme.colorScheme.shadeSecondary
             )
-            if (messageUiState.isMine) {
-                if (messageStatus == MessageStatus.SENT) {
+            if (chats.isMine) {
+                if (chats.status is ChatsScreenState.ChatUiState.Status.Sent) {
                     Icon(
                         painter = painterResource(Res.drawable.ic_message_sent),
                         contentDescription = null,
                         tint = Theme.colorScheme.shadeTertiary
                     )
-                } else if (messageStatus == MessageStatus.READ) {
+                } else if (chats.status is ChatsScreenState.ChatUiState.Status.Read) {
                     Icon(
                         painter = painterResource(Res.drawable.ic_message_read),
                         contentDescription = null,
@@ -123,49 +98,63 @@ fun ChatCard(
                     )
                 }
             } else {
-                if (messageStatus == MessageStatus.SENT) {
-                    Box(
+                if (chats.status is ChatsScreenState.ChatUiState.Status.UnRead) {
+                    Text(
+                        text = "12",
+                        style = Theme.typography.label.small,
+                        color = Theme.colorScheme.primary.onPrimary,
                         modifier = Modifier
                             .clip(CircleShape)
                             .background(
                                 color = Theme.colorScheme.brand.brand,
                                 shape = CircleShape
                             )
-                            .size(20.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "$balance",
-                            style = Theme.typography.label.small,
-                            color = Theme.colorScheme.primary.onPrimary
-                        )
-                    }
+                            .size(20.dp)
+                    )
                 }
             }
         }
     }
 
-}
-
-@OptIn(ExperimentalUuidApi::class)
-@Composable
-@Preview
-private fun ChatCardPreview() {
-    MenaTheme {
-        ChatCard(
-            name = "Mona Ayman",
-            lastMessage = "hi my name is mona",
-            isMenaUser = true,
-            messageStatus = MessageStatus.READ,
-            messageUiState = TextMessageUiState(
-                sendTime = LocalDateTime.now(),
-                status = MessageStatusUiState.READ,
-                isMine = false,
-                text = ""
-            ),
-            onClick = {},
-            chatAvatarUrl = "",
-            balance = 100.0
-        )
+    @Composable
+    private fun RowScope.NameAndLastMessage(chats: ChatsScreenState.ChatUiState) {
+        Column(
+            modifier = Modifier.padding(vertical = Theme.spacing._4).weight(1f)
+                .padding(end = Theme.spacing._4),
+            verticalArrangement = Arrangement.spacedBy(Theme.spacing._2)
+        ) {
+            Text(
+                text = chats.name,
+                style = Theme.typography.label.medium,
+                color = Theme.colorScheme.shadePrimary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = chats.lastMessage,
+                style = Theme.typography.label.small,
+                color = Theme.colorScheme.shadeTertiary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
     }
-}
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Composable
+    @Preview
+    private fun ChatCardPreview() {
+        MenaTheme {
+            ChatCard(
+                chats = ChatsScreenState.ChatUiState(
+                    id = Uuid.random(),
+                    name = "mona",
+                    imageUrl = "https://i.ibb.co/DjJLNHm/ef7bf477a8366d411f62a575dc169f0858ca1fec.jpg",
+                    lastMessage = "https://i.ibb.co/DjJLNHm/ef7bf477a8366d411f62a575dc169f0858ca1fec.jpg",
+                    time = "12:34 AM",
+                    status = ChatsScreenState.ChatUiState.Status.Sent,
+                    isMine = true,
+                )
+            )
+        }
+    }
