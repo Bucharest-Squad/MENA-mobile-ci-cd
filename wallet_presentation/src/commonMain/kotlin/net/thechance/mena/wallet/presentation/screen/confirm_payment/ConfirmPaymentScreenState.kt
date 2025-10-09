929549968fb487b97468aa182db77f5057271f2c
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-09 15:36:08 +0300

    refactor: PaymentUiState

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/confirm_payment/ConfirmPaymentScreenState.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/confirm_payment/ConfirmPaymentScreenState.kt
@@ -10,10 +10,10 @@ data class ConfirmPaymentScreenState(
     val isPayBtnLoading: Boolean = false
 ){
     data class PaymentUiState(
-        val amount: String = "530,320",
-        val receiverName: String = "Ahmed Ali",
-        val receiverImage: String? = "https://media.istockphoto.com/id/469738422/photo/large-boulders-on-lake-shore-at-sunset-minnesota-usa.jpg?s=612x612&w=0&k=20&c=4FzViDygZ8CgixTqt3VOudLJUP8uoSeh2UlD_qHYkAw=",
-        val status: TransactionStatus = TransactionStatus.SUCCESS,
-        val balance: String = "1,230.25"
+        val amount: String = "",
+        val receiverName: String = "",
+        val receiverImage: String? = null,
+        val status: TransactionStatus = TransactionStatus.FAILED,
+        val balance: String = ""
     )
 }
