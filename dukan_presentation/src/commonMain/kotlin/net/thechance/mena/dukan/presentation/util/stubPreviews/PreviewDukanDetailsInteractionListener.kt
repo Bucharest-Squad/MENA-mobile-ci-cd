411b1b0abed6679c0cc083c2add0b020864b0ad9
Mohamed Ashraf <ma1261327@gmail.com>
2025-10-10 14:50:37 +0300

    - edit size of icon in products header - fix preview object for dukan details listener

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/stubPreviews/PreviewDukanDetailsInteractionListener.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/stubPreviews/PreviewDukanDetailsInteractionListener.kt
@@ -1,12 +1,17 @@
 package net.thechance.mena.dukan.presentation.util.stubPreviews
 
+import net.thechance.mena.dukan.presentation.util.pagination.Pager
+import net.thechance.mena.dukan.presentation.util.pagination.PagingConfig
+import net.thechance.mena.dukan.presentation.util.pagination.PagingSource
 import net.thechance.mena.dukan.presentation.viewModel.dukanDetails.DukanDetailsInteractionListener
+import net.thechance.mena.dukan.presentation.viewModel.dukanDetails.DukanDetailsUiState
 
 object PreviewDukanDetailsInteractionListener : DukanDetailsInteractionListener {
     override fun onBackClicked() {
     }
 
-    override fun onShelfClicked(id: String) {
+    override fun onShelfClicked(id: String): Pager<Int, DukanDetailsUiState.ProductUiState> {
+        return pager
     }
 
     override fun onViewAllShelfProductsClicked(id: String, name: String) {
@@ -14,4 +19,19 @@ object PreviewDukanDetailsInteractionListener : DukanDetailsInteractionListener 
 
     override fun onViewDukanOnMapClicked(latitude: Double, longitude: Double) {
     }
+
+    override fun productsShelfView(id: String): Pager<Int, DukanDetailsUiState.ProductUiState> {
+        return pager
+    }
+
+    val pager = Pager(
+        config = PagingConfig(),
+        pagingSourceFactory = {
+            object : PagingSource<Int, DukanDetailsUiState.ProductUiState>() {
+                override suspend fun load(params: LoadParams<Int>): LoadResult<Int, DukanDetailsUiState.ProductUiState> {
+                    return LoadResult.Page(emptyList(), null, null)
+                }
+            }
+        },
+    )
 }
