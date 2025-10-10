a064ac82bbe6a2cf7895ee4b057b98456837e09e
Hager Abobakr <hagarabobakr1@gmail.com>
2025-10-10 09:09:08 +0300

    feat: add initial payment result screen

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/payment_result/PaymentResultViewModel.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/payment_result/PaymentResultViewModel.kt
@@ -1,7 +1,9 @@
 package net.thechance.mena.wallet.presentation.screen.payment_result
 
 import net.thechance.mena.wallet.presentation.base.BaseViewModel
+import org.koin.android.annotation.KoinViewModel
 
+@KoinViewModel
 class PaymentResultViewModel() : BaseViewModel<PaymentResultScreenState, PaymentResultEffect>(
     PaymentResultScreenState()
 ), PaymentResultInteractionListener {
