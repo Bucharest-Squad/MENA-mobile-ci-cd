5db4abe4e03d04b2cd765e8722ad37041d86b0b2
nada <nadamansoursamir@gmail.com>
2025-10-11 17:59:34 +0300

    remove unused import

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanDetails/components/ProductCard.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanDetails/components/ProductCard.kt
@@ -1,14 +1,6 @@
 package net.thechance.mena.dukan.presentation.screen.dukanDetails.components
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.SizeTransform
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -21,10 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -111,7 +99,7 @@ fun ProductCard(
 
                 AnimatedContent(
                     targetState = productUiState.showProductQuantity,
-                    transitionSpec ={ fadeTransitionSpec() } ,
+                    transitionSpec = { fadeTransitionSpec() },
                     label = "CartToQuantity"
                 ) {
                     if (it) {
