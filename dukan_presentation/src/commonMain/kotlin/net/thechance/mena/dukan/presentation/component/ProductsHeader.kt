b803e85b062a52b24fe44d007664eb4eb397fd8a
Mohamed Ashraf <ma1261327@gmail.com>
2025-10-12 14:31:09 +0300

    replace products header from dukan details component to presentation component

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/ProductsHeader.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/ProductsHeader.kt
@@ -1,4 +1,4 @@
-package net.thechance.mena.dukan.presentation.screen.dukanDetails.components
+package net.thechance.mena.dukan.presentation.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
