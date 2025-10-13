46077195d8bb078d5532121df38edd85b1a6bc66
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-10-13 16:34:08 +0300

    add datetime dependency

--- identity_presentation/build.gradle.kts
+++ identity_presentation/build.gradle.kts
@@ -52,6 +52,7 @@ kotlin {
             implementation(libs.bundles.coil)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.datetime)
         }
         jvmTest.dependencies {
             implementation(libs.kotlin.test)
