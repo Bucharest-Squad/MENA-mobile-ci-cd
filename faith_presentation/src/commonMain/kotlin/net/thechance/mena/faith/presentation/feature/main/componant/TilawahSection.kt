f2fb953b378229943df33852dc0ba17534b17ccd
Abdallah Bakhet <abakhet2010@gmail.com>
2025-10-16 19:54:13 +0300

    fix: Format 'ayah_number' string resource with an argument

--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/main/componant/TilawahSection.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/main/componant/TilawahSection.kt
@@ -89,7 +89,7 @@ fun TilawahSection(
                         color = Theme.colorScheme.shadePrimary
                     )
                     Text(
-                        text = tilawahUiState?.ayahNumber ?: stringResource(Res.string.ayah_number),
+                        text = tilawahUiState?.ayahNumber ?: stringResource(Res.string.ayah_number, 1),
                         style = Theme.typography.label.small,
                         color = Theme.colorScheme.shadeSecondary
                     )
