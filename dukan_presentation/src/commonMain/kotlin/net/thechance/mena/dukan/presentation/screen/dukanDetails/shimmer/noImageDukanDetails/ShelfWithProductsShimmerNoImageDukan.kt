d5bbf96a6812634c45afde7a828d8ed73dceea78
Elsayed Magdy <saikoo1239@gmail.com>
2025-10-17 23:44:32 +0300

    refactor: use SkeletonOverlayShape in LoadingProductCard

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanDetails/shimmer/noImageDukanDetails/ShelfWithProductsShimmerNoImageDukan.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanDetails/shimmer/noImageDukanDetails/ShelfWithProductsShimmerNoImageDukan.kt
@@ -1,15 +1,15 @@
 package net.thechance.mena.dukan.presentation.screen.dukanDetails.shimmer.noImageDukanDetails
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
-import net.thechance.mena.dukan.presentation.component.LoadingProductsHeader
 import net.thechance.mena.dukan.presentation.component.productCard.LoadingProductCard
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -19,11 +19,14 @@ fun ShelfWithProductsShimmerNoImageDukan() {
         Modifier.fillMaxWidth()
             .padding(top = Theme.spacing._16, start = Theme.spacing._16, end = Theme.spacing._16)
     ) {
-        LoadingProductsHeader()
-        Spacer(Modifier.height(Theme.spacing._8))
-        LoadingProductCard(backgroundColor = Theme.colorScheme.background.surface)
-        Spacer(Modifier.height(Theme.spacing._16))
-        LoadingProductCard(backgroundColor = Theme.colorScheme.background.surface)
+        LazyVerticalGrid(
+            modifier = Modifier.fillMaxSize(),
+            columns = GridCells.Fixed(1)
+        ) {
+            items(20) {
+                LoadingProductCard()
+            }
+        }
     }
 }
 
