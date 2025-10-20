1bae5a0478259807f84b1addbd218077dabd94b7
Farah315 <fara734isgreat@gmail.com>
2025-10-20 23:12:22 +0300

    refactor: rename transaction mapping functions for clarity and consistency

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_details/TransactionDetailsMapper.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_details/TransactionDetailsMapper.kt
@@ -20,64 +20,64 @@ import net.thechance.mena.wallet.presentation.utils.formatLocalDateTime
 import kotlin.uuid.ExperimentalUuidApi
 
 fun Transaction.toUi() = TransactionDetailsScreenState.TransactionDetailsUiState(
-    id = this.transactionId(),
-    amount = this.amount(),
-    date = this.transactionDate(),
-    userName = this.userName(),
-    otherParty = this.otherParty(),
-    transactionType = this.transactionType(),
-    transactionStatus = this.transactionStatus(),
-    userInfo = this.userInfo(),
-    typeContent = this.typeContent(),
-    otherPartyTitle = this.otherPartyTitle()
+    id = getTransactionId(),
+    amount = getAmount(),
+    date = getTransactionDate(),
+    userName = getUserName(),
+    otherParty = getOtherParty(),
+    transactionType = getTransactionType(),
+    transactionStatus = getTransactionStatus(),
+    userInfo = getUserInfo(),
+    typeContent = getTypeContent(),
+    otherPartyTitle = getOtherPartyTitle()
 )
 
 fun imageBitmapToByteArray(imageBitmap: ImageBitmap): ByteArray =
     imageBitmap.toByteArray(CompressionFormat.PNG, quality = Constants.PNG_QUALITY)
 
-private fun Transaction.transactionId(): String =
+private fun Transaction.getTransactionId(): String =
     Constants.ID_PREFIX + id.toString().substring(0, Constants.ID_LENGTH)
 
-private fun Transaction.amount(): String =
+private fun Transaction.getAmount(): String =
     amount.toString().replace(oldValue = ".", newValue = ",")
 
-private fun Transaction.transactionDate(): String =
+private fun Transaction.getTransactionDate(): String =
     formatLocalDateTime(date = createdAt, outputFormat = "dd MMM yyyy, h:mm a")
 
-private fun Transaction.userName(): String = when (type) {
+private fun Transaction.getUserName(): String = when (type) {
     TransactionType.SENT -> senderName
     TransactionType.RECEIVED -> receiverName
     TransactionType.ONLINE_PURCHASE -> senderName
 }
 
-private fun Transaction.otherParty(): String = when (type) {
+private fun Transaction.getOtherParty(): String = when (type) {
     TransactionType.SENT -> receiverName
     TransactionType.RECEIVED -> senderName
     TransactionType.ONLINE_PURCHASE -> receiverName
 }
 
-private fun Transaction.transactionType(): TransactionTypeUiState = when (type) {
+private fun Transaction.getTransactionType(): TransactionTypeUiState = when (type) {
     TransactionType.SENT -> TransactionTypeUiState.SENT
     TransactionType.RECEIVED -> TransactionTypeUiState.RECEIVED
     TransactionType.ONLINE_PURCHASE -> TransactionTypeUiState.ONLINE_PURCHASE
 }
 
-private fun Transaction.transactionStatus(): TransactionStatusUiState = when (status) {
+private fun Transaction.getTransactionStatus(): TransactionStatusUiState = when (status) {
     TransactionStatus.SUCCESS -> TransactionStatusUiState.SUCCESS
     TransactionStatus.FAILED -> TransactionStatusUiState.FAILED
 }
 
-private fun Transaction.userInfo() = when (type) {
+private fun Transaction.getUserInfo() = when (type) {
     TransactionType.SENT, TransactionType.ONLINE_PURCHASE -> Res.string.from
     TransactionType.RECEIVED -> Res.string.to
 }
 
-private fun Transaction.typeContent() = when (type) {
+private fun Transaction.getTypeContent() = when (type) {
     TransactionType.SENT, TransactionType.RECEIVED -> Res.string.transfer
     TransactionType.ONLINE_PURCHASE -> Res.string.purchase
 }
 
-private fun Transaction.otherPartyTitle() = when (type) {
+private fun Transaction.getOtherPartyTitle() = when (type) {
     TransactionType.SENT -> Res.string.to
     TransactionType.RECEIVED -> Res.string.from
     TransactionType.ONLINE_PURCHASE -> Res.string.receiver
