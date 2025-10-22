f022ee9deb56f78f1e1d12e914b88b2042ceba03
Hager Abobakr <hagarabobakr1@gmail.com>
2025-10-23 00:15:02 +0300

    refactor: remove ColumnScope from DetailsInfo composable

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_details/component/DetailsInfo.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_details/component/DetailsInfo.kt
@@ -28,7 +28,7 @@ import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-internal fun ColumnScope.DetailsInfo(
+internal fun DetailsInfo(
     title: String,
     content: String,
     modifier: Modifier = Modifier,
