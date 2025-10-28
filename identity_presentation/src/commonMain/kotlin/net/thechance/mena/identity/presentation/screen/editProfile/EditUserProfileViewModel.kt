b00753923bed7a59ba12ef808448d5c02fffd0bf
Aboud-B <78660324+ABDULLAHHG@users.noreply.github.com>
2025-10-28 01:58:54 -0700

    Update EditUserProfileViewModel.kt

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/editProfile/EditUserProfileViewModel.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/editProfile/EditUserProfileViewModel.kt
@@ -212,7 +212,7 @@ class EditUserProfileViewModel(
             function = ::requestCameraPermission,
             onSuccess = { onCameraPermissionSuccess() },
             onError = ::handleCameraPermissionError,
-            dispatcher = Dispatchers.Main
+            dispatcher = dispatcher
         )
     }
 
