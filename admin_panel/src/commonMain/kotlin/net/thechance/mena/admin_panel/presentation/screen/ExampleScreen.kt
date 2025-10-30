a7cc826e47a56163f9c3d2a5e0092d92b14f8642
Menna Srour <mennasrour441@gmail.com>
2025-10-30 20:22:57 +0300

    refactor: remove the ui for test

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/ExampleScreen.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/ExampleScreen.kt
@@ -5,22 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import net.thechance.mena.admin_panel.presentation.screen.logout.LogoutInteractionListener
-import net.thechance.mena.admin_panel.presentation.screen.logout.LogoutScreen
 import net.thechance.mena.designsystem.presentation.component.button.Button
 import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 
 @Composable
 fun ExampleScreen(){
-    var isLogoutDialogHidden by remember { mutableStateOf(true) }
-
     Column(
         modifier = Modifier
             .background(Theme.colorScheme.background.surface)
@@ -28,22 +20,8 @@ fun ExampleScreen(){
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Button(onClick = {isLogoutDialogHidden = false}) {
+        Button(onClick = {}) {
             Text("Click me", style = Theme.typography.body.large)
         }
-    }
-
-    if (!isLogoutDialogHidden) {
-        LogoutScreen(
-            interactionListener = object : LogoutInteractionListener {
-                override fun onDismissLogoutDialog() {
-                    isLogoutDialogHidden = true
-                }
-
-                override fun onConfirmLogout() {
-                    isLogoutDialogHidden = true
-                }
-            }
-        )
     }
 }
