82e62f4b4fdc69c91026068121f41712fa9e4145
Menna Srour <mennasrour441@gmail.com>
2025-11-01 07:55:58 +0200

    refactor: refactor admin dialog to use basic dialog of the design system

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/component/AdminConfirmationDialog.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/component/AdminConfirmationDialog.kt
@@ -4,11 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -16,46 +17,68 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import net.thechance.mena.admin_panel.resources.Res
 import net.thechance.mena.admin_panel.resources.cancel
 import net.thechance.mena.designsystem.presentation.component.button.OutlinedButton
 import net.thechance.mena.designsystem.presentation.component.button.PrimaryButton
+import net.thechance.mena.designsystem.presentation.component.dialog.BasicDialog
 import net.thechance.mena.designsystem.presentation.component.icon.Icon
+import net.thechance.mena.designsystem.presentation.component.scaffold.ScaffoldScope
 import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun Dialog(
+fun ScaffoldScope.AdminConfirmationDialog(
     dialogIcon: Painter,
     confirmationIcon: Painter,
     title: String,
     description: String,
     confirmationButtonText: String,
+    isVisible: Boolean,
     onDismiss: () -> Unit,
     onConfirm: () -> Unit,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
-    Dialog(onDismissRequest = onDismiss) {
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .background(
-                    color = Theme.colorScheme.background.surfaceLow,
-                    shape = RoundedCornerShape(24.dp)
+    BasicDialog(
+        onDismiss = onDismiss,
+        isVisible = isVisible,
+        dialogCornerShape = RoundedCornerShape(24.dp),
+        contentPadding = PaddingValues(24.dp),
+        hasDismissButton = false,
+        contentColor = Theme.colorScheme.background.surfaceLow,
+        actionButtons = {
+            Row(
+                modifier = Modifier
+                    .padding(top = 12.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                PrimaryButton(
+                    text = stringResource(Res.string.cancel),
+                    onClick = onDismiss,
                 )
-                .padding(24.dp),
+                OutlinedButton(
+                    text = confirmationButtonText,
+                    onClick = onConfirm,
+                    trailingIcon = confirmationIcon,
+                )
+            }
+        },
+        modifier = modifier.width(400.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(bottom = 12.dp),
             horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             Box(
                 modifier = Modifier
-                    .size(88.dp)
                     .background(
                         color = Theme.colorScheme.background.bgError,
                         shape = CircleShape
-                    ),
+                    ).padding(20.dp),
                 contentAlignment = Alignment.Center
             ) {
                 Icon(
@@ -65,64 +88,18 @@ fun Dialog(
                     tint = Theme.colorScheme.error
                 )
             }
-            DialogContent(
-                title = title,
-                description = description,
-                modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp)
-            )
-            DialogActionsRow(
-                onDismiss = onDismiss,
-                onConfirm = onConfirm,
-                confirmationButtonText = confirmationButtonText,
-                confirmationIcon = confirmationIcon
-            )
+            Column {
+                Text(
+                    text = title,
+                    style = Theme.typography.title.medium,
+                    color = Theme.colorScheme.shadePrimary
+                )
+                Text(
+                    text = description,
+                    style = Theme.typography.body.small,
+                    color = Theme.colorScheme.shadeSecondary
+                )
+            }
         }
-    }
-}
-
-@Composable
-private fun DialogContent(
-    title: String,
-    description: String,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Text(
-            text = title,
-            style = Theme.typography.title.medium,
-        )
-        Text(
-            text = description,
-            style = Theme.typography.body.medium,
-        )
-    }
-}
-
-@Composable
-private fun DialogActionsRow(
-    onDismiss: () -> Unit,
-    onConfirm: () -> Unit,
-    confirmationButtonText: String,
-    confirmationIcon: Painter
-) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        PrimaryButton(
-            text = stringResource(Res.string.cancel),
-            onClick = onDismiss,
-            modifier = Modifier.height(48.dp)
-        )
-
-        OutlinedButton(
-            text = confirmationButtonText,
-            onClick = onConfirm,
-            trailingIcon = confirmationIcon,
-            modifier = Modifier.height(48.dp)
-        )
     }
 }
