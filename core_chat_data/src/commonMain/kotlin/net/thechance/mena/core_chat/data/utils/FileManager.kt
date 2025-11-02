d518ec985c1a0952c5dc6d1d13fc676a70bc0878
AdelTamer <adeltammer@gmail.com>
2025-11-02 12:54:01 +0200

    add expect class for FileManager

---
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/utils/FileManager.kt
@@ -0,0 +1,13 @@
+package net.thechance.mena.core_chat.data.utils
+
+expect class FileManager {
+    fun getDirectory(): String
+
+    fun createDirectory(path: String): Boolean
+
+    suspend fun writeFile(path: String, bytes: ByteArray): Boolean
+
+    fun isFileExists(path: String): Boolean
+
+    fun getFileSize(path: String): Long
+}
