306b96fa52257f8f42c8855a08747a9cd5b3fed7
AdelTamer <adeltammer@gmail.com>
2025-11-02 19:02:33 +0200

    Move FileManager Koin module to commonMain

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/fileManagerModule.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/fileManagerModule.kt
@@ -3,8 +3,6 @@ package net.thechance.mena.core_chat.data.di
 import net.thechance.mena.core_chat.data.utils.FileManager
 import org.koin.dsl.module
 
-val fileManagerAndroidModule = module {
-    single {
-        FileManager()
-    }
+val fileManagerModule = module {
+    single { FileManager() }
 }
