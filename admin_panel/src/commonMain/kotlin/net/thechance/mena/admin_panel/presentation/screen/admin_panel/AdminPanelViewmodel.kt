7101cee62128de430a62a39e60bd9bc0116f3018
Menna Srour <mennasrour441@gmail.com>
2025-11-03 12:06:14 +0200

    feat: call logout function in admin panel view model

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/admin_panel/AdminPanelViewmodel.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/admin_panel/AdminPanelViewmodel.kt
@@ -1,12 +1,23 @@
 package net.thechance.mena.admin_panel.presentation.screen.admin_panel
 
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.thechance.mena.admin_panel.domain.exceptions.NoInternetException
+import net.thechance.mena.admin_panel.domain.repository.authentication.AdminAuthenticationRepository
 import net.thechance.mena.admin_panel.presentation.base.BaseViewModel
 import net.thechance.mena.admin_panel.presentation.base.ErrorState
 import org.koin.android.annotation.KoinViewModel
+import org.koin.core.annotation.Provided
 
 @KoinViewModel
-class AdminPanelViewmodel() : BaseViewModel<AdminPanelScreenState, AdminPanelScreenEffect>(
+class AdminPanelViewmodel(
+    @Provided
+    private val authenticationRepository: AdminAuthenticationRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+
+) : BaseViewModel<AdminPanelScreenState, AdminPanelScreenEffect>(
     AdminPanelScreenState()
 ), AdminPanelInteractionListener {
     override fun onTabSelected(tab: AdminPanelScreenState.CurrentTab) {
@@ -23,8 +34,13 @@ class AdminPanelViewmodel() : BaseViewModel<AdminPanelScreenState, AdminPanelScr
     }
 
     override fun onConfirmLogout() {
-        updateState { it.copy(isLogOutDialogShown = false) }
-        sendEffect(AdminPanelScreenEffect.NavigateToLogInScreen)
+        tryToExecute(
+            callee = ::confirmLogout,
+            onSuccess = { onSuccessLoggedOut() },
+            onError = { onFailureLoggedOut() },
+            dispatcher = dispatcher
+        )
+
     }
 
     override fun mapError(throwable: Throwable): ErrorState {
@@ -48,4 +64,20 @@ class AdminPanelViewmodel() : BaseViewModel<AdminPanelScreenState, AdminPanelScr
             AdminPanelScreenState.CurrentTab.DEPOSIT ->
                 AdminPanelScreenEffect.NavigateToDepositScreen
         }
+
+    private fun onSuccessLoggedOut() {
+        updateState { it.copy(isLogOutDialogShown = false) }
+        sendEffect(AdminPanelScreenEffect.NavigateToLogInScreen)
+    }
+
+    private fun confirmLogout() {
+        viewModelScope.launch {
+            authenticationRepository.logout()
+        }
+    }
+
+    private fun onFailureLoggedOut() {
+        TODO()
+    }
+
 }
