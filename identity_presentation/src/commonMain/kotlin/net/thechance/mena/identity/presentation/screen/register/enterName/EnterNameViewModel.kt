7906bef44bc68731f7fa4031565b66c77816fdc0
AhmadKharfan <plamoplamo12@gmail.com>
2025-11-05 23:20:31 +0000

    pass user details to password screen

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/register/enterName/EnterNameViewModel.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/register/enterName/EnterNameViewModel.kt
@@ -3,48 +3,94 @@ package net.thechance.mena.identity.presentation.screen.register.enterName
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import net.thechance.mena.identity.domain.entity.PhoneNumber
+import net.thechance.mena.identity.domain.exception.AuthenticationException
+import net.thechance.mena.identity.domain.repository.RegisterRepository
 import net.thechance.mena.identity.presentation.base.BaseScreenModel
+import net.thechance.mena.identity.presentation.base.error.ErrorState
+import net.thechance.mena.identity.presentation.base.error.handleAuthenticationException
+import net.thechance.mena.identity.presentation.mapper.mapAuthenticationErrorToMessage
+import net.thechance.mena.identity.presentation.mapper.mapErrorToMessage
+import org.jetbrains.compose.resources.StringResource
 
 class EnterNameViewModel(
-    val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val registerRepository: RegisterRepository,
+    private val phoneNumber: PhoneNumber,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : BaseScreenModel<EnterNameUIState, EnterNameUIEffect>(EnterNameUIState()),
     EnterNameInteractionListener {
 
     override fun onChangeFirstName(name: String) {
-        updateState {
-            val newState = this.copy(firstName = name)
-            copy(
-                firstName = name,
-                isNextEnabled = newState.isValidInput()
-            )
-        }
+        updateState { copy(firstName = name) }
+        updateNextButtonState()
     }
 
     override fun onLastNameChange(name: String) {
-        updateState {
-            val newState = this.copy(lastName = name)
-            copy(
-                lastName = name,
-                isNextEnabled = newState.isValidInput()
-            )
-        }
+        updateState { copy(lastName = name) }
+        updateNextButtonState()
     }
 
     override fun onUsernameChange(username: String) {
         updateState {
-            val newState = this.copy(username = username)
             copy(
                 username = username,
-                isNextEnabled = newState.isValidInput()
+                usernameError = null
+            )
+        }
+        updateNextButtonState()
+    }
+
+    private fun updateNextButtonState() {
+        updateState { copy(isNextEnabled = state.value.isValidInput()) }
+    }
+
+    override fun onClickNext() {
+        if (!state.value.isValidInput()) return
+
+        updateState { copy(isLoading = true, isCheckingUsername = true, errorMessage = null) }
+        tryToExecute(
+            function = { registerRepository.checkUserExistence(state.value.username) },
+            onSuccess = { onUsernameCheckSuccess() },
+            onError = ::onUsernameCheckError,
+            dispatcher = dispatcher
+        )
+    }
+
+    private fun onUsernameCheckSuccess() {
+        updateState { copy(isLoading = false, isCheckingUsername = false) }
+        sendNewEffect(createNavigateToPasswordEffect())
+    }
+
+    private fun createNavigateToPasswordEffect(): EnterNameUIEffect.NavigateToPassword {
+        return EnterNameUIEffect.NavigateToPassword(
+            phoneNumber = phoneNumber,
+            firstName = state.value.firstName,
+            lastName = state.value.lastName,
+            username = state.value.username
+        )
+    }
+
+    private fun onUsernameCheckError(throwable: Throwable) {
+        updateState {
+            copy(
+                isLoading = false,
+                isCheckingUsername = false,
+                errorMessage = mapErrorMessage(throwable)
             )
         }
     }
 
-    override fun onClickNext() {
-        sendNewEffect(EnterNameUIEffect.NavigateToNextStep)
-    }
-
     override fun onClearErrorMessage() {
         updateState { copy(errorMessage = null) }
+    }
+
+    private fun mapErrorMessage(throwable: Throwable): StringResource {
+        return when (throwable) {
+            is AuthenticationException -> mapAuthenticationErrorToMessage(
+                handleAuthenticationException(throwable)
+            )
+
+            else -> mapErrorToMessage(ErrorState.GenericError(throwable))
+        }
     }
 }
