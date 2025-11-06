3e90e5d81a2bd7c2f79bb08e5a627ca7bccc3f0d
AhmadKharfan <plamoplamo12@gmail.com>
2025-11-06 16:09:05 +0000

    add postFileWithDataAndTokens extension function

--- identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/utils/postJson.kt
+++ identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/utils/postJson.kt
@@ -3,8 +3,10 @@ package net.thechance.mena.identity.data.utils
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.request.forms.MultiPartFormDataContent
 import io.ktor.client.request.forms.formData
 import io.ktor.client.request.forms.submitFormWithBinaryData
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
@@ -51,6 +53,38 @@ suspend fun HttpClient.postFileWithData(
             )
         }
     )
+
+    if (response.status != HttpStatusCode.OK) {
+        throw ClientRequestException(response, response.body())
+    }
+}
+
+suspend fun HttpClient.postFileWithDataAndTokens(
+    path: String,
+    fileKey: String,
+    imageByteArray: ByteArray?,
+    accessToken: String
+) {
+    if (imageByteArray == null) return
+
+    val response = this.post {
+        url(path)
+        header(HttpHeaders.Authorization, "Bearer $accessToken")
+        setBody(
+            MultiPartFormDataContent(
+                formData {
+                    append(
+                        key = fileKey,
+                        value = imageByteArray,
+                        headers = Headers.build {
+                            append(HttpHeaders.ContentType, ContentType.Image.JPEG.toString())
+                            append(HttpHeaders.ContentDisposition, "filename=image.jpeg")
+                        }
+                    )
+                }
+            )
+        )
+    }
 
     if (response.status != HttpStatusCode.OK) {
         throw ClientRequestException(response, response.body())
