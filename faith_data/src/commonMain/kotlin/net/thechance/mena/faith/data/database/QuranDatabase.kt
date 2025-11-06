937cc01ed670daefc5eecbc466e49e107bc77898
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-11-07 01:13:45 +0200

    feat: Add data layer for surah audio caching

--- faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/database/QuranDatabase.kt
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/database/QuranDatabase.kt
@@ -5,10 +5,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 
-@Database(entities = [AyahDto::class], version = 1)
+@Database(entities = [AyahDto::class, SurahSoundDto::class], version = 1)
 @ConstructedBy(QuranDatabaseConstructor::class)
 abstract class QuranDatabase : RoomDatabase() {
     abstract fun getAyaDao(): AyahDao
+    abstract fun getSurahDao(): SurahSoundDao
 }
 
 @Suppress("KotlinNoActualForExpect")
