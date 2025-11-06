3a1c061587545c72f87c692fb1b7d887f7dc1c93
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-11-06 22:49:34 +0200

    feat: update navigation from category publish to home

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/category_publish/CategoryPublishScreen.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/category_publish/CategoryPublishScreen.kt
@@ -54,7 +54,8 @@ internal fun CategoryPublishScreen(
         when (effect) {
             is CategoryPublishEffect.NavigateBack -> navController.popBackStack()
             is CategoryPublishEffect.NavigateToHome -> navController.navigate(Route.Home) {
-                popUpTo(Route.MainContainer)
+                popUpTo(Route.Home)
+                launchSingleTop = true
             }
         }
     }
