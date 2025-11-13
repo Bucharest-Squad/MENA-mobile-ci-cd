8b197f0f86fb829399bec02e934eb904a6cf52b2
Menna Srour <mennasrour441@gmail.com>
2025-11-13 18:00:37 +0200

    fix: update DukanQueryParams to use Dukan.Status instead of User.Status

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/domain/model/DukanQueryParams.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/domain/model/DukanQueryParams.kt
@@ -1,11 +1,10 @@
 package net.thechance.mena.admin_panel.domain.model
 
 import net.thechance.mena.admin_panel.domain.entity.dukan.Dukan
-import net.thechance.mena.admin_panel.domain.entity.user.User
 
 data class DukanQueryParams(
     val searchInput: String?,
-    val status: User.Status,
+    val status: Dukan.Status,
     val activationStatus: Dukan.ActivationStatus?,
     val sortType: DukansSortType?,
     val sortDirection: SortDirection?,
