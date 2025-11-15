b3ae68d4f010c7e78bf323354b36da4069c7f626
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-11-15 15:08:35 +0200

    test: Fix `PrivacyAndPolicyScreenViewModelTest`

--- identity_presentation/src/androidUnitTest/kotlin/net/thechance/mena/identity/presentation/screen/privacyAndPolicy/PrivacyAndPolicyScreenViewModelTest.kt
+++ identity_presentation/src/androidUnitTest/kotlin/net/thechance/mena/identity/presentation/screen/privacyAndPolicy/PrivacyAndPolicyScreenViewModelTest.kt
@@ -4,9 +4,11 @@ import app.cash.turbine.test
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.LocalDate
 import net.thechance.mena.identity.domain.exception.UnAuthorizedException
+import net.thechance.mena.identity.domain.model.PrivacyAndPolicy
+import net.thechance.mena.identity.domain.model.Section
 import net.thechance.mena.identity.domain.repository.PrivacyAndPolicyRepository
 import net.thechance.mena.identity.helper.BaseCoroutineTest
 import org.junit.Before
@@ -46,19 +48,35 @@ class PrivacyAndPolicyScreenViewModelTest : BaseCoroutineTest() {
     }
 
     @Test
-    fun `getPrivacyAndPolicy() should update state when get privacy and policy successfully`() = runTest {
-        coEvery { privacyRepository.getPrivacyAndPolicy() } returns emptyList()
-        //TODO:Update this after implementing the endpoint.
-       assertTrue { viewModel.state.value.privacyAndPolicySections.isEmpty() }
-    }
+    fun `getPrivacyAndPolicy() should update state when get privacy and policy successfully`() =
+        runTest {
+            coEvery { privacyRepository.getPrivacyAndPolicy() } returns fakePrivacyAndPolicy
+            testDispatcher.scheduler.advanceUntilIdle()
+            assert(viewModel.state.value.privacyAndPolicySections.isNotEmpty())
+        }
 
     @Test
-    fun `getPrivacyAndPolicy() should update error message when get privacy and policy throws exception`() = runTest {
-        coEvery { privacyRepository.getPrivacyAndPolicy() } throws UnAuthorizedException()
-        testDispatcher.scheduler.advanceUntilIdle()
-        val state = viewModel.state.value
-        assertTrue { state.errorMessage != null}
-    }
-    
+    fun `getPrivacyAndPolicy() should update error message when get privacy and policy throws exception`() =
+        runTest {
+            coEvery { privacyRepository.getPrivacyAndPolicy() } throws UnAuthorizedException()
+            testDispatcher.scheduler.advanceUntilIdle()
+            val state = viewModel.state.value
+            assertTrue { state.errorMessage != null }
+        }
 
+
+    val fakePrivacyAndPolicy = PrivacyAndPolicy(
+        updateDate = LocalDate(2025, 12, 1),
+        sections = listOf(
+            Section(
+                title = "What is Lorem Ipsum?",
+                content = "is simply dummy text of the printing and typesetting industry. " +
+                        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s," +
+                        " when an unknown printer took a galley of type and scrambled it to make a type specimen book." +
+                        " It has survived not only five centuries"
+            )
+
+        )
+
+    )
 }
