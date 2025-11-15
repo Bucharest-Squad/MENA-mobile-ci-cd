0d7715cb9c24b421a04ce714eb76d01a6aa46687
Menna Srour <mennasrour441@gmail.com>
2025-11-15 13:37:03 +0200

    feat: add navigation integration for DukanDetails screen

---
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/dukan_details/DukanDetailsScreen.kt
@@ -0,0 +1,33 @@
+package net.thechance.mena.admin_panel.presentation.screen.dukan_details
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import net.thechance.mena.admin_panel.navigation.LocalNavController
+import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+@Composable
+fun DukanDetailsScreen(
+    dukanId: Uuid
+) {
+    val navController = LocalNavController.current
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Details Screen",
+            style = Theme.typography.title.large,
+            modifier = Modifier.clickable { navController.popBackStack() }
+        )
+    }
+}
