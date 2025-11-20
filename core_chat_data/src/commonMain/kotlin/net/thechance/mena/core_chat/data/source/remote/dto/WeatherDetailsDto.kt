0d3929fba6bd0065c73df575d89a62fc0844bd8b
karrar mohammed <karrar.m.habeeb@gmail.com>
2025-11-21 00:29:40 +0300

    reformat code as suggested in pr comments

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/source/remote/dto/WeatherDetailsDto.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/source/remote/dto/WeatherDetailsDto.kt
@@ -5,8 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class WeatherDetailsDto(
-    @SerialName("currentTemperature") val currentTemperature: Double,
-    @SerialName("minTemperature") val minTemperature: Double,
-    @SerialName("maxTemperature") val maxTemperature: Double,
-    @SerialName("weatherType") val weatherType: String
+    @SerialName("currentTemperature")
+    val currentTemperature: Double,
+    @SerialName("minTemperature")
+    val minTemperature: Double,
+    @SerialName("maxTemperature")
+    val maxTemperature: Double,
+    @SerialName("weatherType")
+    val weatherType: String
 )
