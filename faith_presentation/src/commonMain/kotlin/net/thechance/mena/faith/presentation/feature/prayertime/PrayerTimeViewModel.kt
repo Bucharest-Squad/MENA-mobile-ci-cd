af4adba44d9055c1e753ba4e4074782fe2cbb5fc
Marwan Qashwa <marawankashwa24@gmail.com>
2025-11-23 21:21:04 +0200

    fix: move to next day when next prayer is fajr

--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/prayertime/PrayerTimeViewModel.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/prayertime/PrayerTimeViewModel.kt
@@ -13,6 +13,7 @@ import net.thechance.mena.faith.presentation.utils.IslamicDateCalculator
 import net.thechance.mena.faith.presentation.utils.extentions.prayerTime.calculateNextIslamicDate
 import net.thechance.mena.faith.presentation.utils.extentions.prayerTime.calculatePreviousIslamicDate
 import net.thechance.mena.faith.presentation.utils.extentions.prayerTime.formatCountdown
+import net.thechance.mena.faith.presentation.utils.extentions.prayerTime.getHijriDay
 import net.thechance.mena.identity.domain.entity.Address
 import net.thechance.mena.identity.domain.service.LocationService
 import kotlin.time.Clock
@@ -46,7 +47,12 @@ class PrayerTimeViewModel(
 
     private fun onGetUserLocationSuccess(address: Address) {
         currentAddress = address
-        updateState { it.copy(address = address.addressLine) }
+        updateState {
+            it.copy(
+                address = address.addressLine,
+                currentDate = IslamicDate.now(islamicDateCalculator)
+            )
+        }
         getPrayerTimes(address, Clock.System.now())
     }
 
@@ -63,13 +69,9 @@ class PrayerTimeViewModel(
         tryToExecute(
             execute = {
                 val filteredPrayerTimes = prayerTimes.filter { it.name != PrayerName.SUNRISE }
-
-                val currentIslamicDate = IslamicDate.now(islamicDateCalculator)
-
                 updateState {
                     it.copy(
                         prayerTimes = filteredPrayerTimes,
-                        currentDate = currentIslamicDate,
                     )
                 }
             },
@@ -85,6 +87,10 @@ class PrayerTimeViewModel(
             onEmitNewValue =
                 { nextPrayer ->
                     nextPrayer?.let {
+                        if (nextPrayer.name == PrayerName.FAJR && uiState.value.isTodayPrayer) {
+                            getNextDayPrayerTimesIfNoMoreInCurrent(nextPrayer)
+                        }
+
                         startCountdownTimer(nextPrayer = it, address = address)
                     } ?: run {
                         updateState { state ->
@@ -98,6 +104,15 @@ class PrayerTimeViewModel(
             onError = ::handleErrorSnackBar,
             dispatcher = dispatcher
         )
+    }
+
+    private fun getNextDayPrayerTimesIfNoMoreInCurrent(prayerTime: PrayerTime) {
+        val currentDate = uiState.value.currentDate
+        val prayerTimeDay = getHijriDay(prayerTime)
+        if (currentDate.day >= prayerTimeDay) return
+        val nextIslamicDate = currentDate.copy(day = currentDate.day.inc())
+        updateState { it.copy(currentDate = nextIslamicDate) }
+        getPrayerTimesForIslamicDate(nextIslamicDate)
     }
 
     private fun startCountdownTimer(nextPrayer: PrayerTime, address: Address) {
@@ -123,7 +138,7 @@ class PrayerTimeViewModel(
         val currentDate = uiState.value.currentDate
         val previousIslamicDate = calculatePreviousIslamicDate(currentDate)
 
-        updateState { it.copy(currentDate = previousIslamicDate) }
+        updateState { it.copy(currentDate = previousIslamicDate, isTodayPrayer = false) }
 
         getPrayerTimesForIslamicDate(previousIslamicDate)
     }
@@ -132,7 +147,7 @@ class PrayerTimeViewModel(
         val currentDate = uiState.value.currentDate
         val nextIslamicDate = calculateNextIslamicDate(currentDate)
 
-        updateState { it.copy(currentDate = nextIslamicDate) }
+        updateState { it.copy(currentDate = nextIslamicDate, isTodayPrayer = false) }
 
         getPrayerTimesForIslamicDate(nextIslamicDate)
     }
