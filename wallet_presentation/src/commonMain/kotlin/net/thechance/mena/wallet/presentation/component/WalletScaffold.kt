07a72d013a8bdbe2ce1b2ea1092485e8c0027308
Malak Raef <malak.raef20@gmail.com>
2025-11-28 05:11:06 +0200

    feat: add dark mode support for no internet image

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/WalletScaffold.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/WalletScaffold.kt
@@ -10,13 +10,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import mena.wallet_presentation.generated.resources.Res
+import mena.wallet_presentation.generated.resources.ic_error_dark
+import mena.wallet_presentation.generated.resources.img_filter_error
 import mena.wallet_presentation.generated.resources.img_no_internet
 import mena.wallet_presentation.generated.resources.no_internet_content
+import mena.wallet_presentation.generated.resources.no_internet_dark
 import mena.wallet_presentation.generated.resources.no_internet_title
 import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
 import net.thechance.mena.designsystem.presentation.component.scaffold.ScaffoldScope
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.wallet.presentation.base.ErrorState
+import net.thechance.mena.wallet.presentation.navigation.LocalDarkTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
@@ -33,6 +37,11 @@ fun WalletScaffold(
     onRetry: (() -> Unit)? = null,
     content: @Composable () -> Unit
 ) {
+    val noInternetImage =
+        if (LocalDarkTheme.current) painterResource(Res.drawable.no_internet_dark)
+        else
+            painterResource(Res.drawable.img_no_internet)
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -46,17 +55,19 @@ fun WalletScaffold(
                 when {
                     errorState is ErrorState.NoInternet -> {
                         ErrorView(
-                            image = painterResource(Res.drawable.img_no_internet),
+                            image = noInternetImage,
                             title = stringResource(Res.string.no_internet_title),
                             description = stringResource(Res.string.no_internet_content),
                             onRetry = onRetry ?: {}
                         )
                     }
+
                     isLoading -> {
                         Box(modifier = Modifier.fillMaxSize()) {
                             ThreeDotsLoadingIndicator(modifier = Modifier.align(Alignment.Center))
                         }
                     }
+
                     else -> {
                         content()
                     }
